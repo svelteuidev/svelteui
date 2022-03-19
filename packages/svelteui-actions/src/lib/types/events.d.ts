f684b929b43635d8de706a25f861c3215b08d929
Margarida Silva <ms@platforme.com>
2022-03-19 11:03:13 +0000

    feat: use download action

--- packages/svelteui-actions/src/lib/types/events.d.ts
+++ packages/svelteui-actions/src/lib/types/events.d.ts
@@ -2,5 +2,7 @@ declare namespace svelte.JSX {
 	interface HTMLProps<T> {
 		onuseclipboard?: (event: CustomEvent<string>) => void;
 		'onuseclipboard-error'?: (event: CustomEvent<string>) => void;
+		onusedownload?: (event: CustomEvent<Blob>) => void;
+		'onusedownload-error'?: (event: CustomEvent<Blob>) => void;
 	}
 }
