877120d5d1c356edfa81cb596cb4a9d8a59a6039
Kamell Perry <kperry@ascend-innovations.com>
2022-03-22 14:05:43 -0500

    [docs] Minor Updates: Minor updates to actions & fixed nav on homepage

--- docs/src/routes/__layout.svelte
+++ docs/src/routes/__layout.svelte
@@ -2,12 +2,15 @@
 	import { SvelteuiWrapper } from '@svelteuidev/core';
 	import { browser } from '$app/env';
 	import { page } from '$app/stores';
+	import { showSideBar } from '$lib/stores/sidebar';
 	import Navbar from '$lib/Components/Head/Navbar.svelte';
+	import SideBar from '$lib/Components/Head/SideBar.svelte';
 	import '../app.css';
 
 	let isDark: boolean =
 		browser && document.documentElement.classList.contains('dark') ? true : false;
 
+	$: sideBar = $showSideBar;
 	$: val = $page.url.pathname.split('/');
 	$: path = val[val.length - 1];
 	$: checkTheme = (event: CustomEvent) => {
@@ -25,6 +28,11 @@
 
 <SvelteuiWrapper themeObserver={isDark ? 'dark' : 'light'} ssr>
 	<Navbar on:toggle={(e) => checkTheme(e)} />
+	{#if sideBar}
+		<div class={!sideBar ? 'hidden' : 'md:hidden'}>
+			<SideBar sidebar={sideBar} />
+		</div>
+	{/if}
 	<div id="page" class="mt-20 mb-40">
 		<slot />
 	</div>
--- docs/src/routes/docs/__layout.svelte
+++ docs/src/routes/docs/__layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import Toc from '$components/Test/Toc/Toc.svelte';
 	import SideBar from '$lib/Components/Head/SideBar.svelte';
-	import { showSideBar } from '$lib/stores/sidebar';
 	import { page } from '$app/stores';
 	import { ActivityLog } from 'radix-icons-svelte';
 
@@ -9,7 +8,6 @@
 
 	$: val = $page.url.pathname.split('/');
 	$: path = val[val.length - 1];
-	$: sideBar = $showSideBar;
 	$: toggleToc = (path) => {
 		if (path === 'docs' || path === 'contribute') {
 			return 'xl:hidden';
@@ -32,11 +30,6 @@
 	<div class="hidden md:block">
 		<SideBar />
 	</div>
-	{#if sideBar}
-		<div class={!sideBar ? 'hidden' : 'md:hidden'}>
-			<SideBar sidebar={sideBar} />
-		</div>
-	{/if}
 	<main
 		class="z-0 max-w-full px-4 prose prose-zinc dark:prose-invert prose-hr:border-gray-800 prose-a:text-brand hover:prose-a:text-brand-dark prose-tr:flex prose-th:flex-1 prose-td:flex-1 prose-blockquote:text-sm prose-blockquote:text-gray-500 prose-blockquote:border-gray-700 prose-p:text-black dark:prose-p:text-white xl:col-start-1 xl:col-end-4 xl:ml-64"
 		bind:this={main}
