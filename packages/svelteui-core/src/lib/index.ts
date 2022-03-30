cf3f0c7bde3ab896b97adab4ab025883294c643d
Margarida Silva <ms@platforme.com>
2022-03-30 16:27:45 +0100

    [@svelteui/core] Switch: add new component switch

--- packages/svelteui-core/src/lib/index.ts
+++ packages/svelteui-core/src/lib/index.ts
@@ -2,5 +2,6 @@ export * from './_internal';
 export * from './_styles';
 
 export { default as Button } from './Button/Button.svelte';
-export { default as Loader } from './Loader/Loader.svelte';
 export { default as Code } from './Code/Code.svelte';
+export { default as Loader } from './Loader/Loader.svelte';
+export { default as Switch } from './Switch/Switch.svelte';
