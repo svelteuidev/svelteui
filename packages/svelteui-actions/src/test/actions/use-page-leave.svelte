52476a033ebdc48f8bcad1697db4abe2de420c01
Margarida Silva <ms@platforme.com>
2022-04-10 19:29:02 +0100

    [@svelteui/actions] unit testing or use-tab-leave and use-page-leave

---
+++ packages/svelteui-actions/src/test/actions/use-page-leave.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { pageleave } from "$lib/use-page-leave/use-page-leave";
+
+    export let callback;
+</script>
+    
+<div use:pageleave={() => callback()}>
+    Leave
+</div>
