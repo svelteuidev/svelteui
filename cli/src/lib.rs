2fce0e7e86f921798bc53bad5f199d1aa42085f7
Kamell Perry <kperry@ascend-innovations.com>
2022-04-14 01:19:50 -0500

    [core]: finished cli test

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,39 +1,65 @@
-use std::fs;
+use std::env;
 use std::error::Error;
+use std::fs;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{}", line)
     }
 
-    return Ok(())
+    return Ok(());
 }
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
-impl Config {   
+impl Config {
     pub fn new(args: &[String]) -> Result<Config, &str> {
         if args.len() < 3 {
             return Err("Not enough arguments were passed");
         }
-        
+
         let query: String = args[1].clone();
         let filename: String = args[2].clone();
-        
-        return Ok(Config {query, filename})
+        let case_sensitive: bool = env::var("CASE_INSENSITIVE").is_err();
+
+        return Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        });
     }
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
-    
+
     for line in contents.lines() {
         if line.contains(query) {
+            results.push(line)
+        }
+    }
+
+    return results;
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query: String = query.to_lowercase();
+    let mut results: Vec<&str> = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
             results.push(line)
         }
     }
@@ -46,12 +72,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query: &str = "duct";
         let contents: &str = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
@@ -65,6 +92,9 @@ safe, fast, productive.
 Pick three.
 Trust me.";
 
-        assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,12 +1,12 @@
+use cli::Config;
 use std::env;
 use std::process;
-use cli::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("There was a problem parsing arguments: {}", err);
+        eprintln!("There was a problem parsing arguments: {}", err);
         process::exit(1);
     });
 
@@ -14,7 +14,7 @@ fn main() {
     println!("In file: {}", config.filename);
 
     if let Err(e) = cli::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
