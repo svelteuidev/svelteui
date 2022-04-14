7157353668f3299696224d525159951494d43c60
Margarida Silva <ms@platforme.com>
2022-04-14 10:50:00 +0100

    [@svelteui/utilities] Time: format code

--- packages/svelteui-utilities/src/lib/utils/time/time.ts
+++ packages/svelteui-utilities/src/lib/utils/time/time.ts
@@ -2,10 +2,10 @@ export const dateTimeString = (
 	timestamp: number,
 	locale: string = undefined,
 	options: Intl.DateTimeFormatOptions = {
-		dateStyle: "short",
-		timeStyle: "short"
+		dateStyle: 'short',
+		timeStyle: 'short'
 	}
 ): string => {
 	const date = new Date(timestamp);
 	return new Intl.DateTimeFormat(locale, options).format(date);
-}
+};
--- packages/svelteui-utilities/src/test/time.test.ts
+++ packages/svelteui-utilities/src/test/time.test.ts
@@ -5,19 +5,23 @@ import { dateTimeString } from '$lib';
 describe('time', () => {
 	test('returns a date time string based on locale', () => {
 		let timestamp = Date.parse('25 Apr 1974 22:55:00 GMT');
-		let result = dateTimeString(timestamp, "en-US");
+		let result = dateTimeString(timestamp, 'en-US');
 		expect(result).eq('4/25/74, 11:55 PM');
 
-		result = dateTimeString(timestamp, "en-US", { dateStyle: "full", timeStyle: "medium" });
+		result = dateTimeString(timestamp, 'en-US', { dateStyle: 'full', timeStyle: 'medium' });
 		expect(result).eq('Thursday, April 25, 1974 at 11:55:00 PM');
 
-		result = dateTimeString(timestamp, "en-US", { dateStyle: "medium", timeStyle: "medium" });
+		result = dateTimeString(timestamp, 'en-US', { dateStyle: 'medium', timeStyle: 'medium' });
 		expect(result).eq('Apr 25, 1974, 11:55:00 PM');
 
-		result = dateTimeString(timestamp, "pt", { dateStyle: "full", timeStyle: "medium" });
+		result = dateTimeString(timestamp, 'pt', { dateStyle: 'full', timeStyle: 'medium' });
 		expect(result).eq('quinta-feira, 25 de abril de 1974 23:55:00');
 
-		result = dateTimeString(timestamp, "pt", { dateStyle: "full", timeStyle: "medium", timeZone: "utc" });
+		result = dateTimeString(timestamp, 'pt', {
+			dateStyle: 'full',
+			timeStyle: 'medium',
+			timeZone: 'utc'
+		});
 		expect(result).eq('quinta-feira, 25 de abril de 1974 22:55:00');
 	});
 });
