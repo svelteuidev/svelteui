903480b12309214bbf85ed9fc4d9a8d7068d68f8
Margarida Silva <ms@platforme.com>
2022-04-14 11:09:26 +0100

    [@svelteui/utilities] Time: added undefined type to locale

--- packages/svelteui-utilities/src/lib/utils/time/time.ts
+++ packages/svelteui-utilities/src/lib/utils/time/time.ts
@@ -1,6 +1,6 @@
 export const dateTimeString = (
 	timestamp: number,
-	locale: string = undefined,
+	locale: string | undefined = undefined,
 	options: Intl.DateTimeFormatOptions = {
 		dateStyle: 'short',
 		timeStyle: 'short'
