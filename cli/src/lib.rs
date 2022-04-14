1016d9b175a2cc10d94d6272a1d5ddfd6861a19b
Kamell Perry <kperry@ascend-innovations.com>
2022-04-13 20:08:22 -0500

    [core]: cli experimentation

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,8 +1,70 @@
+use std::fs;
+use std::error::Error;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    for line in search(&config.query, &contents) {
+        println!("{}", line)
+    }
+
+    return Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {   
+    pub fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments were passed");
+        }
+        
+        let query: String = args[1].clone();
+        let filename: String = args[2].clone();
+        
+        return Ok(Config {query, filename})
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line)
+        }
+    }
+
+    return results;
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn one_result() {
+        let query: &str = "duct";
+        let contents: &str = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query: &str = "rUsT";
+        let contents: &str = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
     }
 }
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,20 @@
-// SvelteUI Official CLI will be written in Rust.
+use std::env;
+use std::process;
+use cli::Config;
 
 fn main() {
-    
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("There was a problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for: {}", config.query);
+    println!("In file: {}", config.filename);
+
+    if let Err(e) = cli::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
