217cfc82330d1c2d534bc057c2fc9b1a539c6664
Kamell Perry <kperry@ascend-innovations.com>
2022-04-14 11:46:14 -0500

    [core]: moved tests to seperate folder cli

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -66,35 +66,3 @@ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a st
 
     return results;
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn case_sensitive() {
-        let query: &str = "duct";
-        let contents: &str = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Duct tape.";
-
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
-    }
-
-    #[test]
-    fn case_insensitive() {
-        let query: &str = "rUsT";
-        let contents: &str = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Trust me.";
-
-        assert_eq!(
-            vec!["Rust:", "Trust me."],
-            search_case_insensitive(query, contents)
-        );
-    }
-}
