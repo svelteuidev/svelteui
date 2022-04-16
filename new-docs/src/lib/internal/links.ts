7cbe002a2a97466dc42bc7e51f44ec5da2f23458
Kamell Perry <kperry@ascend-innovations.com>
2022-04-16 02:53:22 -0500

    [docs]: all pages mapped out

--- new-docs/src/lib/internal/links.ts
+++ new-docs/src/lib/internal/links.ts
@@ -11,11 +11,19 @@ const changelog = ['0-5-0', '0-5-5'];
 
 const theming = ['svelteui-provider', 'override', 'create-styles', 'dark-theme', 'ssr'];
 
-const actions = ['use-click-outside', 'use-clipboard', 'use-css-variable', 'use-focus'];
+const actions = [
+	'use-click-outside',
+	'use-clipboard',
+	'use-css-variable',
+	'use-focus',
+	'use-download',
+	'use-page-leave',
+	'use-tab-leave'
+];
 
-const core = ['button', 'code', 'loader'];
+const core = ['button', 'code', 'loader', 'action-icon', 'image', 'switch', 'text', 'title'];
 
-const utilities = ['os', 'rafFn'];
+const utilities = ['os', 'raf-fn'];
 
 const motion = ['typewriter', 'flipboard'];
 
