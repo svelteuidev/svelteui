29fb48e0a7d7f04e35e281dfe6aca5e2f076d7b7
Kamell Perry <kperry@ascend-innovations.com>
2022-04-17 20:16:49 -0500

    [docs]: switched to adapter static

--- docs/svelte.config.js
+++ docs/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { kitDocsPlugin } from '@svelteness/kit-docs/node';
 import Icons from 'unplugin-icons/vite';
 import preprocess from 'svelte-preprocess';
