ec8cf3d94c780e48a272d1018a7d39c00e9af526
Kamell Perry <kperry@ascend-innovations.com>
2022-04-25 01:58:57 -0500

    cli: removed target from git

--- cli/target/debug/deps/cli-126529c6a9240307.d
+++
@@ -1,5 +0,0 @@
-/Users/kamell/projects/personal/svelteui/cli/target/debug/deps/cli-126529c6a9240307.rmeta: src/main.rs
-
-/Users/kamell/projects/personal/svelteui/cli/target/debug/deps/cli-126529c6a9240307.d: src/main.rs
-
-src/main.rs:
