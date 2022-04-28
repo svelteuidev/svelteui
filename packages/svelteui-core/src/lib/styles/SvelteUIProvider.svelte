01bfa742fd712950ab4e6cf18841054f849391e2
Kamell Perry <kperry@ascend-innovations.com>
2022-04-28 02:51:32 -0500

    core: added override to Provider

--- packages/svelteui-core/src/lib/styles/SvelteUIProvider.svelte
+++ packages/svelteui-core/src/lib/styles/SvelteUIProvider.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
-	import { dark, theme, getCssText, NormalizeCSS, SvelteUIGlobalCSS } from './index';
-	import type { ColorScheme } from './types';
+	import { dark, theme, css, getCssText, NormalizeCSS, SvelteUIGlobalCSS } from './index';
+	import type { ColorScheme, Override } from './types';
 
 	export let themeObserver: ColorScheme = 'light';
-	export let withNormalizeCSS: boolean = false
-	export let withGlobalStyles: boolean = false
+	export let withNormalizeCSS: boolean = false;
+	export let withGlobalStyles: boolean = false;
 	export let ssr: boolean = false;
+	export let override: Override['props'] = {};
 
 	// weird behavior occurs if the logic is not stored in a variable
 	const ssrStyles = `<style\tid="stitches">${getCssText()}<\/style>`; //eslint-disable-line
-	const globalStylesLight = `<style\tid="svelteui-inject" type="text\/css">body{background-color: white;color: black;}<\/style>` //eslint-disable-line
-	const globalStylesDark = `<style\tid="svelteui-inject" type="text\/css">body{background-color: #1A1B1E;color: #C1C2C5;}<\/style>` //eslint-disable-line
+	const globalStylesLight = `<style\tid="svelteui-inject" type="text\/css">body{background-color: white;color: black;}<\/style>`; //eslint-disable-line
+	const globalStylesDark = `<style\tid="svelteui-inject" type="text\/css">body{background-color: #1A1B1E;color: #C1C2C5;}<\/style>`; //eslint-disable-line
+	const ProviderStyles = css({});
 
 	$: if (withNormalizeCSS) NormalizeCSS();
-	SvelteUIGlobalCSS()
+	SvelteUIGlobalCSS();
 </script>
 
 <svelte:head>
@@ -29,6 +31,6 @@
 	{/if}
 </svelte:head>
 
-<div class="{themeObserver === 'light' ? theme : dark}">
+<div class="{ProviderStyles({ css: override })} {themeObserver === 'light' ? theme : dark}">
 	<slot />
 </div>
