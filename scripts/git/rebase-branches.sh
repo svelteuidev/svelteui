47f86b6c6b780070f7e95bff38b14827c236dbbe
Kamell <71530812+Brisklemonade@users.noreply.github.com>
2022-05-01 19:29:01 -0500

    [core]: added more git scripts (#38)
    * [core] Script to aggregate type files in types folder in each package
    
    * [core] refactor: move type script to scripts root folder
    
    * [core] Script to bump versions in all packages, root and docs
    
    * [core] Bump script ignores docs bump version
    
    * [core] generate-types: rename script and command and moved types folder generation inside package
    
    * [core]: update changesets & eslint preset
    
    * [core]: fixed relative paths in bump scripts
    
    * [core]: removed all build related script as they are not needed
    
    * [core]: release prepare works
    
    * [core]: added repo rebase script
    
    Co-authored-by: Margarida Silva <ms@platforme.com>

--- scripts/git/rebase-branches.sh
+++ scripts/git/rebase-branches.sh
@@ -1,7 +1,12 @@
 git checkout main
 git pull
+
 git checkout next-minor
 git pull
+git rebase main
+git push
+
 git checkout experimental
 git pull
-git checkout main
+git rebase next-minor
+git push
