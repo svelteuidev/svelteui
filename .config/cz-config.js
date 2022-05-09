f59ed259721f5007512948de40aa6e5d0b69c407
Kamell Perry <kperry@ascend-innovations.com>
2022-05-09 04:07:33 -0500

    [core]: config changes
    updated commitizen config

--- .config/cz-config.js
+++ .config/cz-config.js
@@ -24,23 +24,9 @@ module.exports = {
 	ticketNumberPrefix: 'TICKET-',
 	ticketNumberRegExp: '\\d{1,5}',
 
-	// it needs to match the value for field type. Eg.: 'fix'
-	/*
-    scopeOverrides: {
-      fix: [
-        {name: 'merge'},
-        {name: 'style'},
-        {name: 'e2eTest'},
-        {name: 'unitTest'}
-      ]
-    },
-    */
 	// override the messages, defaults are as follows
 	messages: {
 		type: "Select the type of change that you're committing:",
-		scope: '\nDenote the SCOPE of this change (optional):',
-		// used if allowCustomScopes is true
-		customScope: 'Denote the SCOPE of this change:',
 		subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
 		body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
 		breaking: 'List any BREAKING CHANGES (optional):\n',
@@ -51,7 +37,7 @@ module.exports = {
 	allowCustomScopes: true,
 	allowBreakingChanges: ['feat', 'fix'],
 	// skip any questions you want
-	skipQuestions: ['body'],
+	skipQuestions: ['breaking'],
 
 	// limit subject length
 	subjectLimit: 100
