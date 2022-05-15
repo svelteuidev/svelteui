c5596133836ae23b0930140aa66b6e9cab7799b4
Evgeniy Pelmenev <g.snake345@gmail.com>
2022-05-14 20:38:11 -0500

    [@svelteui-demos]: add BooleanControl

---
+++ packages/svelteui-demos/src/lib/components/Configurator/controls/BooleanControl.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { DemoControlProps } from '$lib/types';
+	import { Switch } from '@svelteuidev/core';
+	import { createEventDispatcher } from 'svelte';
+
+	export let value: boolean;
+	export let label: DemoControlProps['label'];
+
+	const dispatch = createEventDispatcher();
+
+	function onChange(e) {
+		dispatch('change', e.currentTarget.checked);
+	}
+</script>
+
+<Switch size="sm" checked={value} {label} on:change={onChange} />
--- packages/svelteui-demos/src/lib/components/Configurator/controls/index.ts
+++ packages/svelteui-demos/src/lib/components/Configurator/controls/index.ts
@@ -1,7 +1,9 @@
 import SelectControl from './SelectControl.svelte';
 import ColorControl from './ColorControl.svelte';
+import BooleanControl from './BooleanControl.svelte';
 
 export const controls = {
 	select: SelectControl,
-	color: ColorControl
+	color: ColorControl,
+	boolean: BooleanControl
 };
