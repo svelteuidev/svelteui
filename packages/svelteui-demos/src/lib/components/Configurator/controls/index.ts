1c0ed435a60086aa0a8235bde845f5e81b3d9e08
Evgeniy Pelmenev <g.snake345@gmail.com>
2022-05-14 23:30:32 -0500

    [@svelteui-demos]: add SizeControl

--- packages/svelteui-demos/src/lib/components/Configurator/controls/index.ts
+++ packages/svelteui-demos/src/lib/components/Configurator/controls/index.ts
@@ -2,10 +2,12 @@ import SelectControl from './SelectControl.svelte';
 import ColorControl from './ColorControl.svelte';
 import BooleanControl from './BooleanControl.svelte';
 import StringControl from './StringControl.svelte';
+import SizeControl from './SizeControl.svelte';
 
 export const controls = {
 	select: SelectControl,
 	color: ColorControl,
 	boolean: BooleanControl,
-	string: StringControl
+	string: StringControl,
+	size: SizeControl
 };
