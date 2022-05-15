e108eb8be7d87232ac3599fb25f6f0da85af7770
Evgeniy Pelmenev <g.snake345@gmail.com>
2022-05-15 00:37:47 -0500

    [@svelteui-demos]: add SegmentedControl

---
+++ packages/svelteui-demos/src/lib/components/Configurator/controls/SegmentedControl.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { DemoControlProps } from '$lib/types';
+	import { NativeSelect } from '@svelteuidev/core';
+	import { upperFirst } from '$lib/utils';
+
+	export let value: string;
+	export let label: DemoControlProps['label'];
+	export let data: DemoControlProps['data'];
+	export let capitalize: DemoControlProps['capitalize'] = true;
+</script>
+
+<!-- TODO: change NativeSelect to Segmented when it will be implemented -->
+<NativeSelect
+	{value}
+	{label}
+	data={data.map((item) => ({
+		value: item.value,
+		label: capitalize ? upperFirst(item.label) : item.label
+	}))}
+	on:change
+/>
--- packages/svelteui-demos/src/lib/components/Configurator/controls/index.ts
+++ packages/svelteui-demos/src/lib/components/Configurator/controls/index.ts
@@ -3,11 +3,13 @@ import ColorControl from './ColorControl.svelte';
 import BooleanControl from './BooleanControl.svelte';
 import StringControl from './StringControl.svelte';
 import SizeControl from './SizeControl.svelte';
+import SegmentedControl from './SegmentedControl.svelte';
 
 export const controls = {
 	select: SelectControl,
 	color: ColorControl,
 	boolean: BooleanControl,
 	string: StringControl,
-	size: SizeControl
+	size: SizeControl,
+	segmented: SegmentedControl
 };
