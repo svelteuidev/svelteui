32b9a4a9554d86e782b281a1ad79fca06501a589
Margarida Silva <ms@platforme.com>
2022-05-15 21:39:14 +0100

    [@svelteui/core]: fix anchor types and ran format

--- packages/svelteui-core/src/lib/components/Anchor/Anchor.styles.ts
+++ packages/svelteui-core/src/lib/components/Anchor/Anchor.styles.ts
@@ -1,3 +1,8 @@
+import type { Override } from '$lib/styles';
 import type { TextProps } from '../Text/Text.styles';
 
-export interface AnchorProps extends TextProps {}
+export interface AnchorProps extends TextProps {
+	className: string;
+	override: Override['props'];
+	root: TextProps['root'];
+}
