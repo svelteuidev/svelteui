82616d5b5d9fd0cc60e69a898750358687d2012c
Kamell Perry <kperry@ascend-innovations.com>
2022-05-16 18:55:35 -0500

    [@svelteui/demos]: added prism to demos

--- packages/svelteui-demos/src/routes/__layout.svelte
+++ packages/svelteui-demos/src/routes/__layout.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
-	import { SvelteUIProvider, Container } from '@svelteuidev/core';
+	import { SvelteUIProvider, Container, Title, Button, Group } from '@svelteuidev/core';
+
+	let isDark = false;
+	function toggleTheme() {
+		isDark = !isDark;
+	}
 </script>
 
-<SvelteUIProvider withGlobalStyles withNormalizeCSS>
+<SvelteUIProvider withGlobalStyles withNormalizeCSS themeObserver={isDark ? 'dark' : 'light'}>
+	<Group override={{ my: '$5' }} position="center">
+		<Button on:click={toggleTheme} variant="outline">
+			{isDark ? 'Dark' : 'Light'} Mode
+		</Button>
+		<Title weight="bold" override={{ letterSpacing: '$tight' }}>
+			This is the testing route for the demos
+		</Title>
+	</Group>
+	<hr />
 	<Container>
 		<slot />
 	</Container>
