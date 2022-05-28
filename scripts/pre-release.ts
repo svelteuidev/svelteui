32f0b4f017b4eeb9a70860ec3bcd68b9d6b0409e
Kamell Perry <kperry@ascend-innovations.com>
2022-05-27 21:18:50 -0500

    [core]: fixed pre-release script

--- scripts/pre-release.ts
+++ scripts/pre-release.ts
@@ -1,16 +1,13 @@
-import { expandGlob } from "https://deno.land/std@0.95.0/fs/mod.ts";
+import { expandGlob } from 'https://deno.land/std@0.95.0/fs/mod.ts';
 
 async function preparePackages() {
-    for await (const file of expandGlob(`./packages/*/package.json`)) {
-        const json = JSON.parse(await Deno.readTextFile(file.path));
+	for await (const file of expandGlob(`./packages/**/package/package.json`)) {
+		const json = JSON.parse(await Deno.readTextFile(file.path));
 
-        json.module = "index.js";
+		json.module = 'index.js';
 
-        await Deno.writeTextFile(
-            file.path,
-            JSON.stringify(json, null, "\t")
-        );
-    }
+		await Deno.writeTextFile(file.path, JSON.stringify(json, null, '\t'));
+	}
 }
 
 await preparePackages();
