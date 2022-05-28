495136170757b47a9a1ed494e94ea2a08de0a769
Margarida Silva <ms@platforme.com>
2022-05-28 15:42:41 +0100

    [@svelteui/demos]: demos for Divider component

---
+++ packages/svelteui-demos/src/lib/demos/core/Divider/Divider.demo.size.svelte
@@ -0,0 +1,38 @@
+<script lang="ts" context="module">
+	import type { CodeDemoType, CodeDemoConfiguration } from '$lib/types';
+
+	const code = `
+    <script>
+        import { Divider } from '@svelteuidev/core';
+    <\/script>
+    
+    <Divider size="xs" />
+    <Divider size="sm" />
+    <Divider size="md" />
+    <Divider size="lg" />
+    <Divider size="xl" />
+    <Divider size={10} />
+	`;
+
+	export const type: CodeDemoType['type'] = 'demo';
+
+	export const configuration: CodeDemoConfiguration = {
+		code
+	};
+</script>
+
+<script lang="ts">
+	import { Divider } from '@svelteuidev/core';
+</script>
+
+<Divider size="xs" />
+
+<Divider size="sm" />
+
+<Divider size="md" />
+
+<Divider size="lg" />
+
+<Divider size="xl" />
+
+<Divider size={10} />
--- packages/svelteui-demos/src/lib/index.ts
+++ packages/svelteui-demos/src/lib/index.ts
@@ -7,8 +7,9 @@ export * as ConfiguratorExamples from './examples/Configurator';
 
 // @svelteui/core
 export * as ButtonDemos from './demos/core/Button';
-export * as ContainerDemos from './demos/core/Container';
 export * as CenterDemos from './demos/core/Center';
+export * as ContainerDemos from './demos/core/Container';
+export * as DividerDemos from './demos/core/Divider';
 
 // Other packages
 export * as PrismDemos from './demos/prism';
