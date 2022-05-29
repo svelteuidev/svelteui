1e0836137196a974e3c954073c5e984de378b081
Kamell Perry <kperry@ascend-innovations.com>
2022-05-29 18:48:43 -0500

    [core]: updated lockfiles for all packages

--- packages/svelteui-core/src/lib/internal/types/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './primitive';
-
-export { LiteralUnion } from './literal-union';
-export { Component } from './Component';
-export { Fn, Function } from './Helpers';
---
+++ packages/svelteui-core/src/lib/internal/types/index.ts
@@ -0,0 +1,4 @@
+export type { Primitive } from './primitive';
+export type { LiteralUnion } from './literal-union';
+export type { Component } from './Component';
+export type { Fn, Function } from './helpers';
