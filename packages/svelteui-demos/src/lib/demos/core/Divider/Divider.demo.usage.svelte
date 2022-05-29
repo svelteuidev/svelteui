79dcbcdf763edd0b3fcccec2fb1aa63c2e966f86
Ana Margarida Silva <ms@platforme.com>
2022-05-29 20:28:24 +0100

    [docs] Divider docs + demos (#81)
    * [@svelteui/core]: add margins to divider
    
    * [@svelteui/core]: specify 'px' in spacings in stitches config
    
    * [@svelteui/demos]: demos for Divider component
    
    * [docs]: docs for Divider component
    
    * [@svelteui/demos]: fix divider demo slot
    
    * [@svelteui/core]: use '
    
    * [@svelteui/core]: revert stitches change and use explicit spacing in divider

---
+++ packages/svelteui-demos/src/lib/demos/core/Divider/Divider.demo.usage.svelte
@@ -0,0 +1,29 @@
+<script lang="ts" context="module">
+	import type { CodeDemoType, CodeDemoConfiguration } from '$lib/types';
+
+	const code = `
+    <script>
+        import { Divider } from '@svelteuidev/core';
+    <\/script>
+    
+    <Divider />
+    <Divider variant='dashed' />
+    <Divider variant='dotted' />
+	`;
+
+	export const type: CodeDemoType['type'] = 'demo';
+
+	export const configuration: CodeDemoConfiguration = {
+		code
+	};
+</script>
+
+<script lang="ts">
+	import { Divider } from '@svelteuidev/core';
+</script>
+
+<Divider />
+
+<Divider variant='dashed' />
+
+<Divider variant='dotted' />
