7da2a942b67fe229acb84d55d73b5ffdbef9103d
Kamell Perry <kperry@ascend-innovations.com>
2022-05-29 21:24:19 -0500

    [docs]: replaced Heading tooltip with SvelteUI version & modified configurator for NumberInput

--- packages/svelteui-demos/src/lib/demos/core/NumberInput/NumberInput.demo.configurator.svelte
+++ packages/svelteui-demos/src/lib/demos/core/NumberInput/NumberInput.demo.configurator.svelte
@@ -14,37 +14,43 @@
 	export const configuration: ConfiguratorDemoConfiguration = {
 		codeTemplate,
 		configurator: [
-            { name: 'placeholder', type: 'string', initialValue: 'Your age' },
-            { name: 'label', type: 'string', initialValue: 'Your age' },
-            { name: 'description', type: 'string' },
-            { name: 'error', type: 'string' },
-            { 
-                name: 'variant',
-                type: 'select',
-                data: [
-                    { label: 'default', value: 'default'},
-                    { label: 'filled', value: 'filled'},
-                    { label: 'unstyled', value: 'unstyled'},
-                    { label: 'headless', value: 'headless'}
-                ],
-                initialValue: 'default',
+			{ name: 'placeholder', type: 'string', initialValue: 'Your age' },
+			{ name: 'label', type: 'string', initialValue: 'Your age' },
+			{ name: 'description', type: 'string' },
+			{ name: 'error', type: 'string' },
+			{
+				name: 'variant',
+				type: 'select',
+				data: [
+					{ label: 'default', value: 'default' },
+					{ label: 'filled', value: 'filled' },
+					{ label: 'unstyled', value: 'unstyled' },
+					{ label: 'headless', value: 'headless' }
+				],
+				initialValue: 'default',
 				defaultValue: 'default'
-            },
-            { name: 'radius', type: 'size', initialValue: 'sm', defaultValue: 'sm' },
+			},
+			{ name: 'radius', type: 'size', initialValue: 'sm', defaultValue: 'sm' },
 			{ name: 'size', type: 'size', initialValue: 'sm', defaultValue: 'sm' },
-            { name: 'disabled', type: 'boolean', initialValue: false, defaultValue: false },
-			{ name: 'required', type: 'boolean', initialValue: false, defaultValue: false },
+			{ name: 'disabled', type: 'boolean', initialValue: false, defaultValue: false },
+			{ name: 'required', type: 'boolean', initialValue: true, defaultValue: true },
 			{ name: 'invalid', type: 'boolean', initialValue: false, defaultValue: false },
-			{ name: 'hideControls', label: 'Hide Controls', type: 'boolean', initialValue: false, defaultValue: false }
+			{
+				name: 'hideControls',
+				label: 'Hide Controls',
+				type: 'boolean',
+				initialValue: false,
+				defaultValue: false
+			}
 		]
 	};
 </script>
 
 <script lang="ts">
-	import type { NumberInputProps } from '@svelteuidev/core';
+	import type { NumberInputStyles } from '@svelteuidev/core';
 	import { NumberInput, Center } from '@svelteuidev/core';
 
-	export let props: NumberInputProps = {};
+	export let props: Partial<NumberInputStyles.NumberInputProps> = {};
 </script>
 
 <Center>
