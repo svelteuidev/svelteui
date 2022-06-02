6f5a1f09711bb49fd78dda350223c46fbe9cbaa4
Margarida Silva <ms@platforme.com>
2022-06-02 23:47:55 +0100

    [@svelteui/demos]: burger demos

---
+++ packages/svelteui-demos/src/lib/demos/core/Burger/Burger.demo.color.svelte
@@ -0,0 +1,32 @@
+<script lang="ts" context="module">
+	import type { CodeDemoType, CodeDemoConfiguration } from '$lib/types';
+
+	const code = `
+<script>
+	import { Burger } from '@svelteuidev/core';
+<\/script>
+
+<Burger />
+<Burger color="#fe6734" />
+<Burger color="#45f50d" />`;
+
+	export const type: CodeDemoType['type'] = 'demo';
+
+	export const configuration: CodeDemoConfiguration = {
+		code
+	};
+</script>
+
+<script lang="ts">
+	import { Burger, Center, SimpleGrid } from '@svelteuidev/core';
+
+    let opened = [];
+</script>
+
+<Center>
+    <SimpleGrid cols={3} override={{ alignItems: 'center' }}>
+        <Burger opened={opened[0]} on:click={() => (opened[0] = !opened[0])} />
+        <Burger color="#fe6734" opened={opened[1]} on:click={() => (opened[1] = !opened[1])} />
+        <Burger color="#45f50d" opened={opened[2]} on:click={() => (opened[2] = !opened[2])} />
+    </SimpleGrid>
+</Center>
--- packages/svelteui-demos/src/lib/index.ts
+++ packages/svelteui-demos/src/lib/index.ts
@@ -8,6 +8,7 @@ export * as ConfiguratorExamples from './examples/Configurator';
 // @svelteui/core
 export * as AffixDemos from './demos/core/Affix';
 export * as AnchorDemos from './demos/core/Anchor';
+export * as BurgerDemos from './demos/core/Burger';
 export * as ButtonDemos from './demos/core/Button';
 export * as CenterDemos from './demos/core/Center';
 export * as ContainerDemos from './demos/core/Container';
