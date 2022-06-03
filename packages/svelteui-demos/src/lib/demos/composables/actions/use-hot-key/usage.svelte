b620ae14582034f03f8320bd7c5b5a330dc822f9
Margarida Silva <ms@platforme.com>
2022-06-03 17:42:50 +0100

    [@svelteui/demos]: hot key action demo

---
+++ packages/svelteui-demos/src/lib/demos/composables/actions/use-hot-key/usage.svelte
@@ -0,0 +1,33 @@
+<script lang="ts" context="module">
+	import type { CodeDemoType, CodeDemoConfiguration } from '$lib/types';
+
+	const code = `
+<script>
+    import { hotkey } from '@svelteuidev/composables';
+
+    function onSubmit() {
+        alert("You've used a hotkey");
+    }
+<\/script>
+
+<p>Press ⌘+Enter or Ctrl+Enter to trigger an alert</p>
+<div use:hotkey={[['mod+Enter', () => onSubmit()]]}/>`;
+
+	export const type: CodeDemoType['type'] = 'demo';
+
+	export const configuration: CodeDemoConfiguration = {
+		code
+	};
+</script>
+
+<script>
+	import { hotkey } from '@svelteuidev/composables';
+
+    function onSubmit() {
+        alert("You've used a hotkey");
+    }
+</script>
+
+
+<p>Press ⌘+Enter or Ctrl+Enter to trigger an alert</p>
+<div use:hotkey={[['mod+Enter', () => onSubmit()]]}/>
