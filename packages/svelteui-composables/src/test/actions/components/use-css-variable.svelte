ba490d8dec5630a2ade7e56b6cbc42825ad249d9
Margarida Silva <ms@platforme.com>
2022-06-15 10:46:52 +0100

    [@svelteui/composables]: port old unit tests to the composables package

---
+++ packages/svelteui-composables/src/test/actions/components/use-css-variable.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { cssvariable } from '$lib/actions/use-css-variable/use-css-variable';
+
+	export let cssVariables = { display: 'block', 'background-color': 'yellow' };
+</script>
+
+<div id="css-variable" use:cssvariable={cssVariables} />
