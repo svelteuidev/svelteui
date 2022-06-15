dd930df2612feac824d4ae8403fa9efc40d8f75c
Ana Margarida Silva <ms@platforme.com>
2022-06-15 17:10:06 +0100

    [@svelteui/composables]: port old unit tests to the composables package (#122)

---
+++ packages/svelteui-composables/src/test/actions/components/use-download.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { download } from '$lib/actions/use-download/use-download';
+
+	export let blob = new Blob([]);
+	export let filename = '';
+	export let callback;
+	export let callbackError;
+</script>
+
+<button
+	id="download"
+	use:download={{ blob: blob, filename: filename }}
+	on:usedownload={(event) => callback(event.detail)}
+	on:usedownload-error={(event) => callbackError(event.detail)}
+>
+	Download
+</button>
---
+++ packages/svelteui-composables/src/test/actions/components/use-page-leave.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { pageleave } from '$lib/actions/use-page-leave/use-page-leave';
+
+	export let callback;
+</script>
+
+<div use:pageleave={() => callback()}>Leave</div>
