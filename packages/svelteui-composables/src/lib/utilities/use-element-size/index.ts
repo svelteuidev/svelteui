a2cae301978f364b4ac0206967646c6087fc52c3
Muhammed Ali CAN <ali.can@via-vis.com>
2022-06-16 16:30:37 +0300

    [@svelteui/utilities]: added use-element-size utility

---
+++ packages/svelteui-composables/src/lib/utilities/use-element-size/index.ts
@@ -0,0 +1,1 @@
+export { useElementSize } from './use-element-size.js';
---
+++ packages/svelteui-composables/src/lib/utilities/use-element-size/use-element-size.ts
@@ -0,0 +1,55 @@
+import { writable } from 'svelte/store';
+import { browser } from '../../shared/utilities/utils/index.js';
+import type { Action, Writable } from '../../shared/utilities/types';
+
+type ObserverRect = Omit<DOMRectReadOnly, 'toJSON'>;
+
+export interface ElementSize {
+	size: Writable<ObserverRect>;
+	elementsize: Action;
+}
+
+const defaultState: ObserverRect = {
+	x: 0,
+	y: 0,
+	width: 0,
+	height: 0,
+	top: 0,
+	left: 0,
+	bottom: 0,
+	right: 0
+};
+
+export function useElementSize(): ElementSize {
+	const size = writable(defaultState);
+
+	return {
+		size,
+		elementsize: (node: HTMLElement) => {
+			let frameID = 0;
+
+			const observer = browser
+				? new ResizeObserver((entries: any) => {
+						const entry = entries[0];
+
+						if (entry) {
+							cancelAnimationFrame(frameID);
+
+							frameID = requestAnimationFrame(() => {
+								size.set(entry.contentRect);
+							});
+						}
+				  })
+				: null;
+
+			observer.observe(node);
+
+			return {
+				destroy: () => {
+					observer.disconnect();
+					cancelAnimationFrame(frameID);
+				}
+			};
+		}
+	};
+}
