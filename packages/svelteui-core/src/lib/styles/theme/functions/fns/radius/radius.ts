ec797ef850845dcb038f9108017e1464d14df05e
Margarida Silva <ms@platforme.com>
2022-06-21 16:29:23 +0100

    [@svelteui/core]: fallback to default theme if no provider was used

--- packages/svelteui-core/src/lib/styles/theme/functions/fns/radius/radius.ts
+++ packages/svelteui-core/src/lib/styles/theme/functions/fns/radius/radius.ts
@@ -1,8 +1,8 @@
-import { useSvelteUIThemeContext } from '../../../SvelteUIProvider';
+import { useSvelteUITheme, useSvelteUIThemeContext } from '../../../SvelteUIProvider';
 import type { SvelteUINumberSize } from '$lib/styles';
 
 export function radius(radii?: SvelteUINumberSize): number | string {
-	const theme = useSvelteUIThemeContext().theme;
+	const theme = useSvelteUIThemeContext()?.theme || useSvelteUITheme();
 
 	if (typeof radii === 'number') {
 		return radii;
