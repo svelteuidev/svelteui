83ef384d2e542002d85d45f191811eb0345f1ba6
Margarida Silva <ms@platforme.com>
2022-06-24 13:52:31 +0100

    [@svelteui/core]: notification component

---
+++ packages/svelteui-core/src/lib/components/Notification/Notification.styles.ts
@@ -0,0 +1,129 @@
+import { createStyles, dark, vFunc } from '$lib/styles';
+import type { DefaultProps, SvelteUIColor, SvelteUINumberSize } from '$lib/styles';
+import type { Component } from '$lib/internal';
+import type { CloseButtonProps } from '../ActionIcon/ActionIcon.styles';
+
+export interface NotificationProps extends DefaultProps {
+	title?: string;
+	color?: SvelteUIColor;
+	radius?: SvelteUINumberSize;
+	loading?: boolean;
+	icon?: Component | HTMLOrSVGElement;
+	iconSize?: number;
+	iconProps?: Record<string, unknown>;
+	withCloseButton?: boolean;
+	closeButtonLabel?: string;
+	closeButtonProps?: CloseButtonProps;
+	onClose?(...args: any[]): any;
+}
+
+export interface NotificationStylesParams {
+	color: SvelteUIColor;
+	radius: SvelteUINumberSize;
+}
+
+export default createStyles((theme, { color, radius }: NotificationStylesParams) => {
+	const _radiusPx = theme.fn.radius(radius);
+	const _radius = parseInt(_radiusPx.toString().split('px')[0]);
+	const topBottom = Math.min(Math.max(_radius / 1.2, 4), 30);
+	const colors = vFunc(color).filled;
+
+	return {
+		root: {
+			boxSizing: 'border-box',
+			position: 'relative',
+			display: 'flex',
+			alignItems: 'center',
+			paddingLeft: 22,
+			paddingRight: 5,
+			paddingTop: theme.space.xsPX.value,
+			paddingBottom: theme.space.xsPX.value,
+			borderRadius: _radius,
+			backgroundColor: theme.colors.white.value,
+			boxShadow: theme.shadows.lg,
+			border: `1px solid ${theme.fn.themeColor('gray', 2)}`,
+			[`${dark.selector} &`]: {
+				backgroundColor: theme.fn.themeColor('dark', 6),
+				border: `1px solid ${theme.fn.themeColor('dark', 6)}`
+			},
+			'&::before': {
+				content: "''",
+				display: 'block',
+				position: 'absolute',
+				width: 6,
+				top: topBottom,
+				bottom: topBottom,
+				left: 4,
+				borderRadius: _radius,
+				backgroundColor: colors.backgroundColor
+			},
+			'&.withLoader': {
+				paddingLeft: theme.space.xsPX.value,
+				'&::before': {
+					display: 'none'
+				}
+			},
+			'&.withIcon': {
+				paddingLeft: theme.space.xsPX.value,
+				'&::before': {
+					display: 'none'
+				},
+				'& .icon': {
+					backgroundColor: colors.backgroundColor,
+					color: theme.colors.white.value
+				}
+			}
+		},
+		icon: {
+			boxSizing: 'border-box',
+			marginRight: theme.space.mdPX.value,
+			width: 28,
+			height: 28,
+			borderRadius: 28,
+			display: 'flex',
+			flex: 'none',
+			alignItems: 'center',
+			justifyContent: 'center',
+			color: theme.colors.white.value
+		},
+		loader: {
+			marginRight: theme.space.mdPX.value
+		},
+		content: {
+			flex: 1,
+			overflow: 'hidden',
+			marginRight: 10
+		},
+		title: {
+			lineHeight: 1.4,
+			marginBottom: 2,
+			overflow: 'hidden',
+			textOverflow: 'ellipsis',
+			color: theme.fn.themeColor('gray', 9),
+			[`${dark.selector} &`]: {
+				color: theme.colors.white.value
+			},
+		},
+		description: {
+			color: theme.fn.themeColor('gray', 6),
+			lineHeight: 1.4,
+			overflow: 'hidden',
+			textOverflow: 'ellipsis',
+			[`${dark.selector} &`]: {
+				color: theme.fn.themeColor('dark', 2)
+			},
+			'&:only-child': {
+				color: theme.colors.black.value,
+				[`${dark.selector} &`]: {
+					color: theme.fn.themeColor('dark', 0)
+				},
+			}
+		},
+		closeButton: {
+			color: theme.colors.black.value,
+			[`${dark.selector} &`]: {
+				color: theme.colors.white.value
+			}
+		}
+	};
+});
---
+++ packages/svelteui-core/src/lib/components/Notification/Notification.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+	import useStyles from './Notification.styles';
+	import { Box } from '../Box';
+	import { CloseButton } from '../ActionIcon';
+    import { Loader } from '../Loader';
+    import { Text } from '../Text';
+	import type { NotificationProps as $$NotificationProps } from './Notification.styles';
+
+	export let use: $$NotificationProps['use'] = [],
+		element: $$NotificationProps['element'] = undefined,
+		className: $$NotificationProps['className'] = 'blue',
+		override: $$NotificationProps['override'] = {},
+        title: $$NotificationProps['title'] = undefined,
+		color: $$NotificationProps['color'] = 'blue',
+		radius: $$NotificationProps['radius'] = 'sm',
+        loading: $$NotificationProps['loading'] = false,
+		icon: $$NotificationProps['icon'] = undefined,
+		iconSize: $$NotificationProps['iconSize'] = 16,
+		iconProps: $$NotificationProps['iconProps'] = {},
+		withCloseButton: $$NotificationProps['withCloseButton'] = true,
+		closeButtonLabel: $$NotificationProps['closeButtonLabel'] = undefined,
+        closeButtonProps: $$NotificationProps['closeButtonProps'] = {},
+		onClose: $$NotificationProps['onClose'] = () => {};
+	export { className as class };
+
+	$: ({ cx, classes, getStyles } = useStyles({ color, radius }));
+</script>
+
+<Box
+	{use}
+	bind:element
+	role="alert"
+	class={cx(className, icon ? 'withIcon' : '', loading ? 'withLoader' : '', getStyles({ css: override }))}
+	{...$$restProps}
+>
+    {#if icon && !loading}
+        <slot class={classes.icon} name="icon">
+            <svelte:component this={icon} class={classes.icon} {...iconProps} />
+        </slot>
+    {/if}
+    {#if loading}
+       <Loader class={classes.loader} size={28} {color} />
+    {/if}
+    <div class={classes.content}>
+        {#if title}
+            <Text class={classes.title} size='sm' weight={500}>
+                {title}
+            </Text>
+        {/if}
+        <Text class={classes.description} size='sm' color='dimmed'>
+            <slot />
+        </Text>
+    </div>
+    {#if withCloseButton}
+        <CloseButton
+            class={classes.closeButton}
+            aria-label={closeButtonLabel}
+            variant='hover'
+            size={28}
+            {iconSize}
+            {...closeButtonProps}
+            on:click={onClose}
+        />
+    {/if}
+</Box>
