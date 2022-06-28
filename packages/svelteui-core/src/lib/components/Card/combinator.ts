a8377cddfae6e3e48103dfef872368bdcfb84e63
Kamell Perry <kperry@ascend-innovations.com>
2022-06-28 17:47:18 -0500

    [@svelteui/core]: ts-ignore on the combinator components

--- packages/svelteui-core/src/lib/components/Card/combinator.ts
+++ packages/svelteui-core/src/lib/components/Card/combinator.ts
@@ -2,6 +2,8 @@ import _Card from './Card.svelte';
 import Section from './CardSection/CardSection.svelte';
 
 // Combine Card and Section
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 _Card.Section = Section;
 const Card = _Card as typeof _Card & { Section: typeof Section };
 
