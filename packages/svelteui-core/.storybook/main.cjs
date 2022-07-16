3f156c3dbb4aa9d05e752cfb4813f1c2262e5da1
Kamell Perry <kperry@ascend-innovations.com>
2022-07-16 12:23:18 -0500

    [@svelteui/core]: configuring story book (viewport modifications and set up)

--- packages/svelteui-core/.storybook/main.cjs
+++ packages/svelteui-core/.storybook/main.cjs
@@ -1,4 +1,6 @@
-const sveltePreprocess = require('svelte-preprocess');
+const preprocess = require('svelte-preprocess');
+const path = require('path');
+const { loadConfigFromFile, mergeConfig } = require('vite');
 
 module.exports = {
 	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
@@ -16,7 +18,7 @@ module.exports = {
 	// 	preprocess: require('../svelte.config.js').preprocess
 	// },
 	svelteOptions: {
-		preprocess: sveltePreprocess({
+		preprocess: preprocess({
 			babel: {
 				presets: [
 					[
@@ -34,6 +36,24 @@ module.exports = {
 		})
 	},
 	features: {
-		storyStoreV7: true
+		// On-demand store does not work for .svelte stories, only CSF.
+		// Requires all stories to be loaded in bulk.
+		// REFERENCE https://storybook.js.org/docs/svelte/configure/overview#feature-flags
+		storyStoreV7: false
+	},
+	async viteFinal(config, { configType }) {
+		const { config: userConfig } = await loadConfigFromFile(
+			path.resolve(__dirname, '../vite.config.js')
+		);
+		// Remove Svelte plugins that would duplicate those added by the Storybook plugin
+		const plugins = userConfig.plugins
+			.flat(1)
+			.filter(
+				(p) => !p.name.startsWith('vite-plugin-svelte') || p.name === 'vite-plugin-svelte-kit'
+			);
+		return mergeConfig(config, {
+			...userConfig,
+			plugins
+		});
 	}
 };
---
+++ packages/svelteui-core/src/lib/components/Button/Button.stories.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import { Button } from './index';
+</script>
+
+<Meta title="Components/Button" component={Button} />
+
+<Template let:args>
+	<Button {...args} />
+</Template>
+
+<Story name="Button" />
