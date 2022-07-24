0d8a8dccdfc974718708e170ce308d025b9da477
BeeMargarida <ams-amargarida@hotmail.com>
2022-07-24 13:07:58 +0100

    [core]: revert some changes and remove logs

--- packages/svelteui-core/src/components/Alert/Alert.stories.svelte
+++ packages/svelteui-core/src/components/Alert/Alert.stories.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
 	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
 	import { Alert } from './index';
-	import { useSvelteUITheme } from '$lib/styles';
-
-	const theme = useSvelteUITheme();
-    console.log("theme", theme);
 </script>
 
 <Meta title="Components/Alert" component={Alert} />
