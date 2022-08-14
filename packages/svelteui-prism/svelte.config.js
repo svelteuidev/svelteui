67eb986fadfaaed0b8d1d3b904728b16bc8f5d3a
BeeMargarida <ams-amargarida@hotmail.com>
2022-08-14 19:14:35 +0100

    [@svelteui/prism]: fix indentation problem on the first line

--- packages/svelteui-prism/svelte.config.js
+++ packages/svelteui-prism/svelte.config.js
@@ -1,17 +1,11 @@
 import mm from 'micromatch';
-import autoprefixer from 'autoprefixer';
-import cssnano from 'cssnano';
 import preprocess from 'svelte-preprocess';
 import fs from 'fs';
 export const pkg = JSON.parse(fs.readFileSync(new URL('package.json', import.meta.url), 'utf8'));
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess({
-		postcss: {
-			plugins: [autoprefixer(), cssnano()]
-		}
-	}),
+	preprocess: preprocess(),
 	compilerOptions: {
 		generate: 'ssr',
 		hydratable: true
