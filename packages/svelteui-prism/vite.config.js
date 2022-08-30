10a20fe79df9235cfff3cd7f8f503c20a607ad7a
BeeMargarida <ams-amargarida@hotmail.com>
2022-08-30 09:18:34 +0100

    [@svelteui/prism]: moved code from src/lib into src

--- packages/svelteui-prism/vite.config.js
+++ packages/svelteui-prism/vite.config.js
@@ -3,9 +3,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
-	deps: {
-		inline: ['radix-icons-svelte']
-	},
 	optimizeDeps: {
 		exclude: ['radix-icons-svelte']
 	}
