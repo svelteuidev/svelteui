f1f2d20e0280e73c73903a74ce50138fde44ec42
Margarida Silva <ms@platforme.com>
2022-09-03 11:16:35 +0100

    [@svelteui/core]: fix Tab dark mode color and simple story for testing purposes

---
+++ packages/svelteui-core/src/components/Tabs/Tabs.stories.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import { Camera, EnvelopeClosed, Gear } from 'radix-icons-svelte';
+	import { Tabs } from './index';
+</script>
+
+<Meta title="Components/Tabs" component={Tabs} />
+
+<Template let:args>
+	<Tabs {...args}>
+		<Tabs.Tab label="Gallery" icon={Camera}>Gallery tab content</Tabs.Tab>
+		<Tabs.Tab label="Messages" icon={EnvelopeClosed}>Messages tab content</Tabs.Tab>
+		<Tabs.Tab label="Settings" icon={Gear}>Settings tab content</Tabs.Tab>
+	</Tabs>
+</Template>
+
+<Story name="Tabs" id="tabs" />
