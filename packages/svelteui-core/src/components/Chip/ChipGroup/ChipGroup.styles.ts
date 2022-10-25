424989165b2959d5fc42738cfee0d67b74f3bbe0
Brian Evans <53117772+mrbrianevans@users.noreply.github.com>
2022-10-25 11:45:02 +0100

    [@svelteui/core]: add Chip and ChipGroup components (#209)
    * [@svelteui/core]: add Chip and ChipGroup components
    
    Stubbed first draft of components. Not ready yet. Mostly copied from other components like Checkbox and also Mantine. Not tested.
    
    Signed-off-by: Brian Evans <ebrian101@gmail.com>
    
    * [@svelteui/core]: change Chip styles after code review
    
    Edited the styles for Chip based on PR feedback.
    Used color functions where possible, instead of hard coding.
    Also changed the way variants are selected.
    Added a checkmark when a chip is selected.
    
    Signed-off-by: Brian Evans <ebrian101@gmail.com>
    
    * [@svelteui/core]: added Storybook story for Chip
    
    Signed-off-by: Brian Evans <ebrian101@gmail.com>
    
    * [@sveltui/core]: make Chip label dynamic
    
    Co-authored-by: Ana Margarida Silva <ams-amargarida@hotmail.com>
    
    * [@svelteui/core]: Chip styling and Story changes
    
    Based on PR feedback:
     - added more stories to storybook for sizing, and the different states
     - fixed dark mode selectors to use old method [`${theme.dark} &`]
     - made the label dynamic from label prop if no children provided to component
    
    Signed-off-by: Brian Evans <ebrian101@gmail.com>
    
    * [@svelteui/core]: hide input element in Chip
    
    Wrap the input element in a div which hides it.
    
    Signed-off-by: Brian Evans <ebrian101@gmail.com>
    
    * [@svelteui/core]: ChipGroup storybook Stories
    
    Added Stories for testing the ChipGroup.
    Removed some defaults from the ChipGroup component since these are set in Chip.
    Removed the input type prop, as it's not necessary.
    
    Signed-off-by: Brian Evans <ebrian101@gmail.com>
    
    * [@svelteui/core]: Chip disabled checked styling
    
    Signed-off-by: Brian Evans <ebrian101@gmail.com>
    
    Signed-off-by: Brian Evans <ebrian101@gmail.com>
    Co-authored-by: Kamell <71530812+Brisklemonade@users.noreply.github.com>
    Co-authored-by: Ana Margarida Silva <ams-amargarida@hotmail.com>

---
+++ packages/svelteui-core/src/components/Chip/ChipGroup/ChipGroup.styles.ts
@@ -0,0 +1,20 @@
+import type {
+	CSS,
+	DefaultProps,
+	SvelteUIColor,
+	SvelteUINumberSize,
+	SvelteUISize
+} from '$lib/styles';
+
+export interface ChipGroupProps extends DefaultProps {
+	color?: SvelteUIColor;
+	items?: { label: string; value: string }[];
+	value?: string[];
+	label?: string;
+	size?: SvelteUISize;
+	radius?: SvelteUINumberSize;
+	direction?: 'row' | 'column';
+	align?: CSS['alignItems'];
+	spacing?: SvelteUINumberSize;
+	wrapperProps?: Record<string, any>;
+}
---
+++ packages/svelteui-core/src/components/Chip/index.ts
@@ -0,0 +1,4 @@
+export { default as Chip } from './Chip.svelte';
+export { default as ChipGroup } from './ChipGroup/ChipGroup.svelte';
+export type { ChipProps, ChipStyleParams } from './Chip.styles.js';
+export type { ChipGroupProps } from './ChipGroup/ChipGroup.styles.js';
