9dd4a5cbc44fcd7611d88e70a2fa51b25926aa1e
Margarida Silva <ana.silva@replai.io>
2022-10-29 17:00:54 +0100

    [@svelteui/demos]: chip demos

---
+++ packages/svelteui-demos/src/demos/core/Chip/Chip.demo.configurator.svelte
@@ -0,0 +1,49 @@
+<script lang="ts" context="module">
+	import type { ConfiguratorDemoType, ConfiguratorDemoConfiguration } from '$lib/types';
+
+	const codeTemplate = (props: string, children: string) => `
+<script>
+  import { Chip } from '@svelteuidev/core';
+<\/script>
+
+<Chip ${props}>Click me</Chip>
+`;
+
+	export const type: ConfiguratorDemoType['type'] = 'configurator';
+
+	export const configuration: ConfiguratorDemoConfiguration = {
+		codeTemplate,
+		configurator: [
+			{
+				name: 'color',
+				type: 'color',
+				initialValue: 'blue',
+				defaultValue: 'blue'
+			},
+			{
+				name: 'variant',
+				type: 'select',
+				data: [
+					{ label: 'filled', value: 'filled' },
+					{ label: 'outline', value: 'outline' }
+				],
+				initialValue: 'outline',
+				defaultValue: 'outline'
+			},
+			{ name: 'radius', type: 'size', initialValue: 'xl', defaultValue: 'xl' },
+			{ name: 'size', type: 'size', initialValue: 'sm', defaultValue: 'sm' },
+			{ name: 'disabled', type: 'boolean', initialValue: false, defaultValue: false }
+		]
+	};
+</script>
+
+<script lang="ts">
+	import type { ChipProps } from '@svelteuidev/core';
+	import { Center, Chip } from '@svelteuidev/core';
+
+	export let props: ChipProps = {};
+</script>
+
+<Center>
+	<Chip checked {...props}>Click me</Chip>
+</Center>
---
+++ packages/svelteui-demos/src/demos/core/Chip/ChipGroup.demo.configurator.svelte
@@ -0,0 +1,87 @@
+<script lang="ts" context="module">
+	import type { ConfiguratorDemoType, ConfiguratorDemoConfiguration } from '$lib/types';
+
+	const codeTemplate = (props: string, children: string) => `
+<script>
+  import { ChipGroup } from '@svelteuidev/core';
+
+  let values;
+<\/script>
+
+<ChipGroup ${props}
+  bind:values
+  items={[
+    {label: 'One', value: "one"},
+    {label: 'Two', value: "two"},
+    {label: 'Three', value: "three"}
+  ]}
+/>
+`;
+
+	export const type: ConfiguratorDemoType['type'] = 'configurator';
+
+	export const configuration: ConfiguratorDemoConfiguration = {
+		codeTemplate,
+		configurator: [
+			{ name: 'multiple', type: 'boolean', initialValue: false, defaultValue: false },
+			{
+				name: 'color',
+				type: 'color',
+				initialValue: 'blue',
+				defaultValue: 'blue'
+			},
+			{
+				name: 'variant',
+				type: 'select',
+				data: [
+					{ label: 'filled', value: 'filled' },
+					{ label: 'outline', value: 'outline' }
+				],
+				initialValue: 'outline',
+				defaultValue: 'outline'
+			},
+			{
+				name: 'position',
+				type: 'select',
+				data: [
+					{ label: 'left', value: 'left' },
+					{ label: 'right', value: 'right' },
+					{ label: 'center', value: 'center' },
+					{ label: 'apart', value: 'apart' }
+				],
+				initialValue: 'left',
+				defaultValue: 'left'
+			},
+			{
+				name: 'direction',
+				type: 'select',
+				data: [
+					{ label: 'row', value: 'row' },
+					{ label: 'column', value: 'column' }
+				],
+				initialValue: 'row',
+				defaultValue: 'row'
+			},
+			{ name: 'spacing', type: 'size', initialValue: 'md', defaultValue: 'md' },
+			{ name: 'radius', type: 'size', initialValue: 'xl', defaultValue: 'xl' },
+			{ name: 'size', type: 'size', initialValue: 'sm', defaultValue: 'sm' },
+			{ name: 'disabled', type: 'boolean', initialValue: false, defaultValue: false }
+		]
+	};
+</script>
+
+<script lang="ts">
+	import type { ChipGroupProps } from '@svelteuidev/core';
+	import { ChipGroup } from '@svelteuidev/core';
+
+	export let props: ChipGroupProps = {};
+</script>
+
+<ChipGroup
+	{...props}
+	items={[
+		{ label: 'One', value: 'one' },
+		{ label: 'Two', value: 'two' },
+		{ label: 'Three', value: 'three' }
+	]}
+/>
