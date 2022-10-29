7ef1df31fcd4b303843d1bec00dc91dd55d934b3
Margarida Silva <ana.silva@replai.io>
2022-10-29 17:00:36 +0100

    [@svelteui/core]: improvements to Chip and ChipGroup - support multiple or not

--- packages/svelteui-core/src/components/Chip/Chip.svelte
+++ packages/svelteui-core/src/components/Chip/Chip.svelte
@@ -19,8 +19,8 @@
 		checked: $$Props['checked'] = false,
 		label: $$Props['label'] = '',
 		radius: $$Props['radius'] = 'xl',
-    size: $$Props['size'] = 'sm',
-    variant: $$Props['variant'] = 'outline',
+		size: $$Props['size'] = 'sm',
+		variant: $$Props['variant'] = 'outline',
 		transitionDuration: $$Props['transitionDuration'] = 100;
 	export { className as class };
 
@@ -46,33 +46,38 @@ A picker for one or more options.
 -->
 
 <Box bind:element class={cx('chip', className, getStyles({ css: override }))} {...$$restProps}>
-  <div class={classes.inputContainer}>
-    <input
-      use:useActions={use}
-      use:forwardEvents
-      bind:checked
-      class={classes.input}
-      class:disabled
-      type='checkbox'
-      {disabled}
-      {value}
-      {id}
-    />
-  </div>
+	<div class={classes.inputContainer}>
+		<input
+			use:useActions={use}
+			use:forwardEvents
+			bind:checked
+			class={classes.input}
+			class:disabled
+			type="checkbox"
+			{disabled}
+			{value}
+			{id}
+		/>
+	</div>
 
-  <label class={cx(classes.label, variant)} class:checked class:disabled for={id}>
-    {#if checked}
-      <div class="{classes.iconWrapper}">
-        <svg class="{classes.checkIcon}" viewBox="0 0 10 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M4 4.586L1.707 2.293A1 1 0 1 0 .293 3.707l3 3a.997.997 0 0 0 1.414 0l5-5A1 1 0 1 0 8.293.293L4 4.586z"
-            fill="currentColor"
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-          />
-        </svg>
-      </div>
-    {/if}
-    <slot>{label}</slot>
-  </label>
+	<label class={cx(classes.label, variant)} class:checked class:disabled for={id}>
+		{#if checked}
+			<div class={classes.iconWrapper}>
+				<svg
+					class={classes.checkIcon}
+					viewBox="0 0 10 7"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M4 4.586L1.707 2.293A1 1 0 1 0 .293 3.707l3 3a.997.997 0 0 0 1.414 0l5-5A1 1 0 1 0 8.293.293L4 4.586z"
+						fill="currentColor"
+						fill-rule="evenodd"
+						clip-rule="evenodd"
+					/>
+				</svg>
+			</div>
+		{/if}
+		<slot>{label}</slot>
+	</label>
 </Box>
--- packages/svelteui-core/src/components/Chip/ChipGroup/ChipGroup.stories.svelte
+++ packages/svelteui-core/src/components/Chip/ChipGroup/ChipGroup.stories.svelte
@@ -1,38 +1,89 @@
 <script lang="ts">
-  import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
-  import { ChipGroup } from '../index';
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import { ChipGroup } from '../index';
 
-  let bindValue // used for testing bind:value
+	let bindValue;
 </script>
 
 <Meta title="Components/Chip/ChipGroup" component={ChipGroup} />
 
 <Template let:args>
-  <ChipGroup {...args}></ChipGroup>
+	<ChipGroup {...args} />
 </Template>
 
-<Story name="ChipGroup" args={{items:[{label:'Chip', value: 'chip'},{label:'Dip', value: 'dip'}]}}/>
+<Story
+	name="ChipGroup"
+	args={{
+		items: [
+			{ label: 'Chip', value: 'chip' },
+			{ label: 'Dip', value: 'dip' }
+		]
+	}}
+/>
+
+<Story name="Allow multiple">
+	<ChipGroup
+		bind:value={bindValue}
+		multiple
+		items={[
+			{ label: 'One', value: 'one' },
+			{ label: 'Two', value: 'two' },
+			{ label: 'Three', value: 'three' }
+		]}
+	/>
+	<p>Variable bound to value: <code>{JSON.stringify(bindValue)}</code></p>
+</Story>
 
 <Story name="Bind value">
-  <ChipGroup bind:value={bindValue} items={[{label: 'One', value: 1},{label: 'Two', value: 2},{label: 'Three', value: 3}]} />
-  <p>Variable bound to value: <code>{JSON.stringify(bindValue)}</code></p>
+	<ChipGroup
+		bind:value={bindValue}
+		items={[
+			{ label: 'One', value: 'one' },
+			{ label: 'Two', value: 'two' },
+			{ label: 'Three', value: 'three' }
+		]}
+	/>
+	<p>Variable bound to value: <code>{JSON.stringify(bindValue)}</code></p>
 </Story>
 
 <Story name="Spacing">
-  {#each ['xs', 'sm', 'md', 'lg', 'xl'] as spacing}
-    <p>Spacing: {spacing}</p>
-    <ChipGroup bind:value={bindValue} {spacing} items={[{label: 'One', value: 1},{label: 'Two', value: 2},{label: 'Three', value: 3}]} />
-  {/each}
+	{#each ['xs', 'sm', 'md', 'lg', 'xl'] as spacing}
+		<p>Spacing: {spacing}</p>
+		<ChipGroup
+			bind:value={bindValue}
+			{spacing}
+			items={[
+				{ label: 'One', value: 'one' },
+				{ label: 'Two', value: 'two' },
+				{ label: 'Three', value: 'three' }
+			]}
+		/>
+	{/each}
 </Story>
 
 <Story name="Directions">
-  {#each ['row', 'column'] as direction}
-    <p>Direction: {direction}</p>
-    <ChipGroup bind:value={bindValue} {direction} items={[{label: 'One', value: 1},{label: 'Two', value: 2},{label: 'Three', value: 3}]} />
-  {/each}
+	{#each ['row', 'column'] as direction}
+		<p>Direction: {direction}</p>
+		<ChipGroup
+			bind:value={bindValue}
+			{direction}
+			items={[
+				{ label: 'One', value: 'one' },
+				{ label: 'Two', value: 'two' },
+				{ label: 'Three', value: 'three' }
+			]}
+		/>
+	{/each}
 </Story>
 
 <Story name="Input label">
-  <ChipGroup bind:value={bindValue} label="Pick as many as you like" description="You can select more than one"
-             items={[{label: 'One', value: 1},{label: 'Two', value: 2},{label: 'Three', value: 3}]} />
+	<ChipGroup
+		bind:value={bindValue}
+		label="Pick as many as you like"
+		items={[
+			{ label: 'One', value: 'one' },
+			{ label: 'Two', value: 'two' },
+			{ label: 'Three', value: 'three' }
+		]}
+	/>
 </Story>
