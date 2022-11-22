c7bfe0fd1e66af32234d983229b30545109fa729
Margarida Silva <ana.silva@replai.io>
2022-11-22 23:42:49 +0000

    [@svelteui/composables]: export use-move

--- packages/svelteui-composables/src/actions/index.ts
+++ packages/svelteui-composables/src/actions/index.ts
@@ -8,6 +8,7 @@ export * from './use-io/index.js';
 export * from './use-lazy/index.js';
 export * from './use-lock-scroll/index.js';
 export * from './use-long-press/index.js';
+export * from './use-move/index.js';
 export * from './use-page-leave/index.js';
 export * from './use-persistent-tab/index.js';
 export * from './use-portal/index.js';
