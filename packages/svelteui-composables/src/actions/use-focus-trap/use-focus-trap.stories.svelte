33d7d51bdb7ee70a52a1d9a007392f7742995849
Margarida Silva <ana.silva@replai.io>
2022-12-08 23:35:05 +0000

    [@svelteui/composables]: story for action focus-trap

---
+++ packages/svelteui-composables/src/actions/use-focus-trap/use-focus-trap.stories.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import { Button, Text, Title, NativeSelect, TextInput } from '@svelteuidev/core';
+  import { focustrap } from './use-focus-trap';
+</script>
+
+<Meta title="Composables/use-focus-trap" />
+
+<Template let:args>
+  <div use:focustrap>
+    <Title>Form</Title>
+    <Text>Please fill out this form</Text>
+    <TextInput
+      placeholder="Your name"
+      label="Full name"
+    />
+    <NativeSelect data={['Svelte', 'React', 'Vue', 'Angular', 'Solid']}
+      placeholder="Pick one"
+      label="Select your favorite framework/library"
+      description="This is anonymous"
+    />
+    <Button>Submit</Button>
+  </div>
+</Template>
+
+<Story name="use-focus-trap" />
