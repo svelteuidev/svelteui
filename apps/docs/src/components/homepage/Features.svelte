d1a83712a6ca07d88ad8336197d535a97b79abb4
Caladan <caladanofdune@gmail.com>
2022-12-09 16:43:53 +0530

    [docs]: Changed the line-height and added animations on scroll in the homepage (#240)
    * [docs]: Changed lineHight of text in homepage to match Features.svelte
    
    * [docs]: Added transitions on scroll for Features.svelte
    
    * [docs]: Changed variable name to improve clarity
    
    * [docs]: Ran tests and linted code
    
    * [docs]: Changed height of wrapper div

--- apps/docs/src/components/homepage/Features.svelte
+++ apps/docs/src/components/homepage/Features.svelte
@@ -1,26 +1,50 @@
 <script>
 	import { features } from '../../data';
 	import { Title, Text, SimpleGrid, ThemeIcon, Center, Stack, Paper } from '@svelteuidev/core';
+  import { fly } from "svelte/transition"
+  import { onMount } from "svelte"
+  let featuresVisible = false
+
+  onMount(() => {
+    const wrapper = document.querySelector("#wrapper")
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          featuresVisible = true
+        }
+      })
+    }, {
+      threshold: 0.5
+    })
+    observer.observe(wrapper)
+  })
+
 </script>
 
-<SimpleGrid
-	breakpoints={[
-		{ minWidth: 1024, cols: 3, spacing: 'md' },
-		{ minWidth: 768, cols: 2, spacing: 'sm' },
-		{ minWidth: 640, cols: 1, spacing: 'sm' }
-	]}
->
-	{#each features as { description, icon, title }}
-		<Paper shadow="xl">
-			<Stack>
-				<Center override={{ jc: 'start', gap: '$10' }} inline>
-					<ThemeIcon variant="gradient" size="xl">
-						<svelte:component this={icon} size={25} />
-					</ThemeIcon>
-					<Title order={3} weight="extrabold">{title}</Title>
-				</Center>
-				<Text size="lg" override={{ lineHeight: '$md' }}>{description}</Text>
-			</Stack>
-		</Paper>
-	{/each}
-</SimpleGrid>
+<div id="wrapper">
+  {#if featuresVisible}
+    <SimpleGrid
+      breakpoints={[
+        { minWidth: 1024, cols: 3, spacing: 'md' },
+        { minWidth: 768, cols: 2, spacing: 'sm' },
+        { minWidth: 640, cols: 1, spacing: 'sm' }
+      ]}
+    >
+      {#each features as { description, icon, title }, i}
+        <div in:fly="{{ y: 200, duration: (i + 1) * 500 }}" style="height: 100%">
+          <Paper shadow="xl">
+            <Stack>
+              <Center override={{ jc: 'start', gap: '$10' }} inline>
+                <ThemeIcon variant="gradient" size="xl">
+                  <svelte:component this={icon} size={25} />
+                </ThemeIcon>
+                <Title order={3} weight="extrabold">{title}</Title>
+              </Center>
+              <Text size="lg" override={{ lineHeight: '$md' }}>{description}</Text>
+            </Stack>
+          </Paper>
+        </div>
+      {/each}
+    </SimpleGrid>
+  {/if}
+</div>
