e5294ee69ad525c9f5779326b4598878c2e6628d
Caladan <94300963+Caladan08@users.noreply.github.com>
2022-12-09 06:39:54 +0530

    [docs]: Changed height of wrapper div

--- apps/docs/src/components/homepage/Features.svelte
+++ apps/docs/src/components/homepage/Features.svelte
@@ -31,7 +31,7 @@
       ]}
     >
       {#each features as { description, icon, title }, i}
-        <div in:fly="{{ y: 200, duration: (i + 1) * 500 }}">
+        <div in:fly="{{ y: 200, duration: (i + 1) * 500 }}" style="height: 100%">
           <Paper shadow="xl">
             <Stack>
               <Center override={{ jc: 'start', gap: '$10' }} inline>
