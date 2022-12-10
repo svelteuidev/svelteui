7044332068bd1d68b3c7f752e1762dbc393a924f
Caladan <94300963+Caladan08@users.noreply.github.com>
2022-12-10 16:53:46 +0530

    [docs]: Hopefully resolved conflict

--- apps/docs/src/components/homepage/Features.svelte
+++ apps/docs/src/components/homepage/Features.svelte
@@ -32,7 +32,7 @@
     >
       {#each features as { description, icon, title }, i}
         <div in:fly="{{ y: 200, duration: (i + 1) * 500 }}" style="height: 100%">
-          <Paper shadow="xl">
+          <Paper shadow="xl" style="height: 100%">
             <Stack>
               <Center override={{ jc: 'start', gap: '$10' }} inline>
                 <ThemeIcon variant="gradient" size="xl">
