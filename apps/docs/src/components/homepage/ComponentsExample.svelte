6a1d038bedc72660833cb0c7940496b33c56ffa0
Caladan <94300963+Caladan08@users.noreply.github.com>
2023-01-04 20:23:25 +0530

    [docs]: Implemented suggested changes

--- apps/docs/src/components/homepage/ComponentsExample.svelte
+++ apps/docs/src/components/homepage/ComponentsExample.svelte
@@ -79,7 +79,7 @@
 			transitionProperty: 'all',
 			transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
 			transitionDuration: '200ms',
-      borderRadius: theme.radii.md.value,
+      borderRadius: theme.radii.sm.value,
 			width: '15rem !important',
 			height: '4rem !important',
 			fontWeight: 'bold !important',
