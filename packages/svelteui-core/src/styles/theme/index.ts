1bd8bf12dacca3c66f1cba22e397ff91f533b2a6
Margarida Silva <ana.silva@replai.io>
2023-01-12 16:38:09 +0000

    [core]: fix export of typings for the theme engine

--- packages/svelteui-core/src/styles/theme/index.ts
+++ packages/svelteui-core/src/styles/theme/index.ts
@@ -1,6 +1,6 @@
 export * from './SvelteUIProvider/index.js';
 export * from './functions/index.js';
 export * from './utils/index.js';
-export * from './types/index.js';
+export * from "./types/index.d";
 
 export { colors, colorNameMap } from './default-colors';
