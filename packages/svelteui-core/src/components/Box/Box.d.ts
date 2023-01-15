36663984b8b14838a136daf991bc201f86f9d583
Margarida Silva <ana.silva@replai.io>
2023-01-15 22:12:51 +0000

    [@svelteui/core]: improve Box typings and add comment about current limitation of svelte-check

--- packages/svelteui-core/src/components/Box/Box.d.ts
+++ packages/svelteui-core/src/components/Box/Box.d.ts
@@ -1,13 +1,31 @@
 import { SvelteComponent, SvelteComponentTyped } from 'svelte';
-import { HTMLAttributes, type HTMLButtonAttributes } from 'svelte/elements';
-import type { CSS, DefaultProps, SvelteUITheme } from '$lib/styles';
-import type { Component, LiteralUnion } from '$lib/internal';
+import {
+	HTMLAnchorAttributes,
+	HTMLAttributes,
+	HTMLBaseAttributes,
+	HTMLButtonAttributes,
+	HTMLFormAttributes,
+	HTMLLabelAttributes
+} from 'svelte/elements';
+import { CSS, DefaultProps, SvelteUITheme } from '$lib/styles';
+import { Component, LiteralUnion } from '$lib/internal';
 
 export interface BoxProps<T extends EventTarget = HTMLElement>
 	extends DefaultProps<HTMLElement | SvelteComponent>,
-		HTMLAttributes<T> {
+		HTMLAttributes<T>,
+		HTMLAnchorAttributes,
+		HTMLBaseAttributes,
+		HTMLButtonAttributes,
+		HTMLLabelAttributes,
+		HTMLFormAttributes,
+		Record<string, unknown> {
 	root?: LiteralUnion<keyof HTMLElementTagNameMap | Component, string>;
 	css?: CSS | ((theme: SvelteUITheme) => CSS);
+
+	// @TODO: follow this issue - https://github.com/sveltejs/language-tools/issues/1825
+	// since it will possibly add this feature in the future, which will remove all
+	// current errors we have with `data-*` props being passed to Box
+	[key: `data-${string}`]: any;
 }
 
 export interface BoxEvents {
