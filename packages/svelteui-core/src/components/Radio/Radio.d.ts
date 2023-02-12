a5c206f0e2ec7c4cf8f54b91ffc17179039d94e9
Margarida Silva <ams-amargarida@hotmail.com>
2023-02-12 21:57:14 +0000

    [@svelteui/core]: cleanup and simplify radio and radio group ogic

--- packages/svelteui-core/src/components/Radio/Radio.d.ts
+++ packages/svelteui-core/src/components/Radio/Radio.d.ts
@@ -2,12 +2,9 @@ import { HTMLAttributes } from 'svelte/elements';
 import { Component } from '$lib/internal';
 import { DefaultProps, SvelteUIColor, SvelteUINumberSize, SvelteUISize } from '$lib/styles';
 
-
-export type RadioContext = Writable<{ 
-	value?: string;
-}>;
-
-export interface RadioProps extends DefaultProps, HTMLAttributes<HTMLElement> {
+export interface RadioProps
+	extends DefaultProps<HTMLInputElement>,
+		HTMLAttributes<HTMLInputElement> {
 	color?: SvelteUIColor;
 	id?: string;
 	name?: string;
@@ -17,6 +14,8 @@ export interface RadioProps extends DefaultProps, HTMLAttributes<HTMLElement> {
 	label?: Component | string;
 	radius?: SvelteUINumberSize | number;
 	size?: SvelteUISize;
-	labelDirection?: 'right' | 'bottom';
+	labelDirection?: 'right' | 'left';
+	error?: boolean;
+	group?: string;
 	isGrouped?: boolean;
 }
