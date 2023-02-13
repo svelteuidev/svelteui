6a5408d0751b1c52a715ba99186ada7507908a23
masagatech <pratikway.90@gmail.com>
2023-02-13 22:43:06 +0530

    New Component Breadcrumb

---
+++ packages/svelteui-core/src/components/Breadcrumb/Breadcrumb.stories.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+	import { useSvelteUITheme, type SvelteUINumberSize } from '$lib/styles';
+	import { Breadcrumb } from './index';
+	import BreadcrumbItem from './BreadcrumbItem/BreadcrumbItem.svelte';
+	import IconRenderer from '../IconRenderer/IconRenderer.svelte';
+	import { Home, Person } from 'radix-icons-svelte';
+
+	const theme = useSvelteUITheme();
+</script>
+
+<Meta title="Components/Breadcrumb" component={Breadcrumb} />
+
+<Template let:args>
+	<Breadcrumb {...args} size={'md'}>
+		<BreadcrumbItem href="https://google.com">Home</BreadcrumbItem>
+		<BreadcrumbItem active={true}>Application List</BreadcrumbItem>
+	</Breadcrumb>
+</Template>
+
+<Story name="Breadcrumb" />
+
+<Story name="Icon">
+	<Breadcrumb size={'md'}>
+		<BreadcrumbItem href="https://google.com"
+			><IconRenderer slot="icon" icon={Home} />
+		</BreadcrumbItem>
+		<BreadcrumbItem
+			><IconRenderer slot="icon" icon={Person} />
+			Application List</BreadcrumbItem
+		><BreadcrumbItem active={true}>View</BreadcrumbItem>
+	</Breadcrumb>
+</Story>
+
+
+<Story name="Separator">
+	<Breadcrumb size="md" separator="→">
+		<BreadcrumbItem href="https://google.com"
+			><IconRenderer slot="icon" icon={Home} />
+		</BreadcrumbItem>
+		<BreadcrumbItem
+			><IconRenderer slot="icon" icon={Person} />
+			Application List</BreadcrumbItem
+		><BreadcrumbItem active={true}>View</BreadcrumbItem>
+	</Breadcrumb>
+</Story>
---
+++ packages/svelteui-core/src/components/Breadcrumb/BreadcrumbItem/BreadcrumbItem.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import useStyles from './BreadcrumbItem.styles';
+	import { Box } from '../../Box'; 
+	import type { BreadcrumbItemProps as $$BreadcrumbItemProps } from './BreadcrumbItem';
+	import type { BreadcrumbContext } from '../Breadcrumb';
+	import { getContext } from 'svelte';
+	import { ctx } from '../Breadcrumb.svelte';
+
+	interface $$Props extends $$BreadcrumbItemProps {}
+
+	export let use: $$Props['use'] = [],
+		element: $$Props['element'] = undefined,
+		className: $$Props['className'] = '',
+		override: $$Props['override'] = {},
+		color: $$Props['color'] = undefined,
+		href: $$Props['href'] = undefined,
+		size: $$Props['size'] = 'md',
+		active: $$Props['active'] = false;
+
+	export { className as class };
+
+	// retrieves the reactive context so that TimelineItem has access
+	// to the Timeline parameters
+	const state: BreadcrumbContext = getContext(ctx);
+
+	let separator = $state.separator;
+	size = $state.size;
+	color = $state.color;
+
+	$: ({ cx, classes, getStyles, theme } = useStyles({ color, size }, { name: 'Breadcrumb' }));
+</script>
+
+<Box bind:element {use} class={cx(className, getStyles({ css: override }))}>
+	{#if href}
+		<div
+			class={cx('wrapper', {
+				active: active
+			})}
+		>
+			<a {href}> <slot name="icon" class="icon" /><span class="innerText"><slot /></span></a>
+			{#if !active}
+				<span class="separator">{separator}</span>
+			{/if}
+		</div>
+	{:else}
+		<div
+			class={cx('wrapper', {
+				active: active
+			})}
+		>
+			<slot name="icon" class="icon" /><span class="innerText"><slot /></span>
+			{#if !active}
+				<span class="separator">{separator}</span>
+			{/if}
+		</div>
+	{/if}
+</Box>
+
+<style>
+	.separator {
+		margin: 0 8px;
+	}
+</style>
