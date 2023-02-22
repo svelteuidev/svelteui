bf983ea8126031cab5ddd8e51127d848349b012f
epelmenev <epelmenev@dealersocket.com>
2023-02-22 17:38:12 -0600

    [@svelteui/core]: make createAriaHider more accurate

--- packages/svelteui-composables/src/actions/use-focus-trap/create-aria-hider.ts
+++ packages/svelteui-composables/src/actions/use-focus-trap/create-aria-hider.ts
@@ -3,28 +3,44 @@ type Value = {
 	ariaHidden: string | null;
 };
 
-export function createAriaHider(
-	containerNode: HTMLElement,
-	selector: string = 'body > :not(script)'
-) {
-	const rootNodes: (Value | undefined)[] = Array.from<HTMLElement>(
-		document.querySelectorAll(selector)
-	).map((node): Value | undefined => {
-		if (node?.shadowRoot?.contains(containerNode) || node.contains(containerNode)) {
+export function createAriaHider(containerNode: HTMLElement) {
+	const hiddenDomNodes: Value[] = [];
+
+	gatherOutsideDomNodes(containerNode);
+	hiddenDomNodes.map(({ node }) => {
+		node.setAttribute('aria-hidden', 'true');
+	});
+
+	function gatherOutsideDomNodes(currentElement: HTMLElement): HTMLElement | undefined {
+		// 1. Start at the current element (begins with the container element).
+		// 2. Hide all sibling elements that are not already aria-hidden.
+		// 3. Go up one level to the parent element.
+		// 4. Repeat steps 2-3 until you reach the `html` element.
+
+		if (!currentElement) {
 			return undefined;
 		}
 
-		const ariaHidden = node.getAttribute('aria-hidden');
+		if (currentElement !== document.querySelector('body')) {
+			const parentElement = currentElement.parentElement as HTMLElement;
+			const siblingElements = [...(Array.from(parentElement.children) as HTMLElement[])];
+			siblingElements.forEach((node) => {
+				const ariaHidden = node.getAttribute('aria-hidden');
+				if (
+					node !== currentElement &&
+					node.nodeName !== 'SCRIPT' &&
+					(ariaHidden === null || ariaHidden === 'false')
+				) {
+					hiddenDomNodes.push({ node, ariaHidden });
+				}
+			});
 
-		if (ariaHidden === null || ariaHidden === 'false') {
-			node.setAttribute('aria-hidden', 'true');
+			gatherOutsideDomNodes(parentElement);
 		}
-
-		return { node, ariaHidden };
-	});
+	}
 
 	return () => {
-		rootNodes.forEach((item) => {
+		hiddenDomNodes.forEach((item) => {
 			if (!item) {
 				return;
 			}
