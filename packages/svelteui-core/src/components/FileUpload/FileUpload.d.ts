4d935e1b50d8787ae0158133bf9b721195bd9337
Pratik Naik @ India <pratikway.90@gmail.com>
2023-02-24 23:19:53 +0530

    [@svelteui/core]: file upload component
    * New Control
    
    * Fixed issues
    
    * Add entry in index
    
    * Update change  Number to number
    
    * [@svelteui/core]: bug fixed

---
+++ packages/svelteui-core/src/components/FileUpload/FileUpload.d.ts
@@ -0,0 +1,38 @@
+import { HTMLAttributes } from 'svelte/elements';
+import { ColorShades, DefaultProps, SvelteUIColor, SvelteUINumberSize } from '$lib/styles';
+
+
+
+export interface FileItem {
+	name?: string;
+	icon?: string;
+	size?: number;
+	file?: File
+
+}
+
+
+export interface FileUploadProps extends DefaultProps<HTMLDivElement>, HTMLAttributes<HTMLElement> {
+	accept?: string;
+	color?: SvelteUIColor;
+	size?: SvelteUINumberSize;
+	label?: string;
+	type?: string = undefined | 'drag';
+	multiple?: boolean;
+	name?: string;
+	files?: FileItem[];
+	icon?: Component | HTMLOrSVGElement;
+	preview?: boolean;
+	resetLabel?: string;
+	reset?: boolean;
+	resetColor?: SvelteUIColor;
+	disabled?: boolean;
+	resetIcon?: Component | HTMLOrSVGElement;
+}
+
+
+export interface FileUploadEvents {
+	selected?: CustomEvent<any>;
+	removed?: CustomEvent<File, number>;
+	reset?: CustomEvent<>;
+}
