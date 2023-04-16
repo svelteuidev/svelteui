d7acce5765e3e4bde07d941fd7e2f5e3465bf9df
Caladan8 <tnambiar2008@gmail.com>
2023-04-16 18:04:10 +0530

    [docs]: Made the search links keyboard focusable

--- apps/docs/src/lib/components/SearchItem.svelte
+++ apps/docs/src/lib/components/SearchItem.svelte
@@ -1,23 +1,36 @@
 <script lang="ts">
-  import { Paper, Group } from "@svelteuidev/core"
-  type SearchType = {
-    title: String,
-    link: String,
-    section?: String
-  }
-  export let search: SearchType
+	import { goto } from '$app/navigation';
+	import { Group } from '@svelteuidev/core';
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	function addSearchLink() {
+		dispatch('addSearch');
+	}
+
+	type SearchType = {
+		title: String;
+		link: String;
+		section?: String;
+	};
+	export let search: SearchType;
 </script>
 
-<Paper class="searchTerm" withBorder>
-  <Group position="apart">
-    <div>
-      <p class="searchItemTitle">{search.title}</p>
-      {#if search.section}
-        <p class="searchItemDescription">{search.section}</p>
-      {/if}
-    </div>
-    <Group position="center" direction="column" override={{ color: "#f5f5f5" }}>
-      <p class="searchLink">{search.link}</p>
-    </Group>
-  </Group>
-</Paper>
+<button
+	class="searchTerm"
+	on:click={() => {
+		addSearchLink();
+		goto(search.link.toString());
+	}}
+>
+	<Group position="apart">
+		<div style="color: black">
+			<p class="searchItemTitle">{search.title}</p>
+			<p class="searchItemDescription">{search.section}</p>
+		</div>
+		<Group position="center" direction="column" override={{ color: '#f5f5f5' }}>
+			<p class="searchLink">{search.link}</p>
+		</Group>
+	</Group>
+</button>
