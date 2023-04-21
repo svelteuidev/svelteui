066cc085a6287ad40c6b709ec5d857ee9c6d50c5
Margarida Silva <ams-amargarida@hotmail.com>
2023-04-21 15:34:19 +0100

    [@svelteui/core]: fix Menu.Item types, related to #350

--- packages/svelteui-core/src/components/Menu/MenuItem/MenuItem.d.ts
+++ packages/svelteui-core/src/components/Menu/MenuItem/MenuItem.d.ts
@@ -1,14 +1,16 @@
-import { HTMLAttributes } from 'svelte/elements';
+import { HTMLAnchorAttributes, HTMLAttributes, HTMLButtonAttributes } from 'svelte/elements';
 import { Component, LiteralUnion } from '$lib/internal';
 import { DefaultProps, SvelteUIColor } from '$lib/styles';
 
-export interface SharedMenuItemProps extends DefaultProps, HTMLAttributes<HTMLElement> {
+export interface SharedMenuItemProps extends DefaultProps, HTMLAttributes<HTMLButtonElement> {
 	color?: SvelteUIColor;
 	disabled?: boolean;
 	icon?: Component | HTMLOrSVGElement;
 	iconProps?: Record<string, any>;
 }
 
-export interface MenuItemProps extends SharedMenuItemProps {
+export interface MenuItemPropsInternal extends SharedMenuItemProps {
 	root?: LiteralUnion<keyof HTMLElementTagNameMap | Component, string>;
 }
+
+export type MenuItemProps = MenuItemPropsInternal & HTMLButtonAttributes & HTMLAnchorAttributes;
