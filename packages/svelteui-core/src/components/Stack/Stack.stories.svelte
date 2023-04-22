36817507f3fd22a8cb934d2c98cbd93f39dbea84
Margarida Silva <ams-amargarida@hotmail.com>
2023-04-22 13:58:31 +0100

    [@svelteui/core]: story for Stack

---
+++ packages/svelteui-core/src/components/Stack/Stack.stories.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import { Button } from '../Button';
+	import { Stack } from './index';
+</script>
+
+<Meta title="Components/Stack" component={Stack} />
+
+<Template let:args>
+	<Stack {...args}>
+		<Button variant="outline">1</Button>
+		<Button variant="outline">2</Button>
+		<Button variant="outline">3</Button>
+	</Stack>
+</Template>
+
+<Story name="Stack" id="stackStory" />
