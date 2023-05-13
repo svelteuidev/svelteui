fb947395202ffb3615923d809d965d97b36abdc2
Margarida Silva <ams-amargarida@hotmail.com>
2023-05-13 11:36:31 +0100

    [fix](core): remove old docs scripts and update docs pub script

--- scripts/docs/strings-to-search-for.ts
+++
@@ -1,7 +0,0 @@
-export const mainDeletion =
-	'a:hover{text-decoration:underline}label{display:block}button,input,select,textarea{font-family:inherit;font-size:inherit;padding:.4em;margin:0 0 .5em;box-sizing:border-box;border:1px solid var(--light);border-radius:2px}input:disabled{color:var(--light)}input[type=range]{height:0}button{color:#333;background-color:#f4f4f4;outline:none}button:active{background-color:#ddd}button:focus{border-color:#666}';
-export const bundleCodeNowrap =
-	'code{font-family:var(--font-mono);font-size:.9rem;background:var(--tag);border:1px solid var(--tag-border);color:var(--tag-text);padding:0 .2em;border-radius:.2em;white-space:nowrap}';
-/** temporary solution, should use regex instead */
-export const properCode =
-	'code{font-family:var(--font-mono);font-size:.9rem;background:var(--tag);border:1px solid var(--tag-border);color:var(--tag-text);padding:0 .2em;border-radius:.2em;white-space:wrap}';
