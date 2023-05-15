2616e489d9f68fc9768b3101bd47b33364a7d762
Margarida Silva <ams-amargarida@hotmail.com>
2023-05-15 22:09:36 +0100

    [refactor](@svelteui/motion): remove routes and organize similar to other packages

--- packages/svelteui-motion/src/lib/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './dist/index.js';
-
-import type {} from './dist/animations/types';
-import type { DefaultParams } from './dist/transitions/types';
-export type { DefaultParams };
--- packages/svelteui-motion/svelte.config.js
+++ packages/svelteui-motion/svelte.config.js
@@ -4,7 +4,12 @@ import preprocess from 'svelte-preprocess';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess()
+	preprocess: preprocess(),
+  kit: {
+		files: {
+			lib: 'src'
+		}
+	}
 };
 
 export default config;
