1991a5f7c6e8c3ac2c0647e9671472760beb21ce
Margarida Silva <ams-amargarida@hotmail.com>
2023-06-03 00:07:35 +0100

    [feat](@svelteui/core): add additional stories for Button and ActionIcon

--- packages/svelteui-core/src/components/ActionIcon/ActionIcon.stories.svelte
+++ packages/svelteui-core/src/components/ActionIcon/ActionIcon.stories.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+  import { useSvelteUITheme } from '$lib/styles';
+  import { Group } from '../Group';
 	import { ActionIcon } from './index';
+
+	const theme = useSvelteUITheme();
+	const colors = Object.keys(theme.colorNames);
 </script>
 
 <Meta title="Components/ActionIcon" component={ActionIcon} />
@@ -9,4 +14,34 @@
 	<ActionIcon {...args} />
 </Template>
 
+<Template id="variants" let:args>
+  <Group mt="xl">
+    <ActionIcon {...args} variant="hover" />
+    <ActionIcon {...args} variant="filled" />
+    <ActionIcon {...args} variant="outline" />
+    <ActionIcon {...args} variant="light" />
+    <ActionIcon {...args} variant="default" />
+    <ActionIcon {...args} variant="transparent" />
+  </Group>
+</Template>
+
 <Story name="ActionIcon" id="actionIconStory" />
+
+<Story name="Colors" id="actionIconColorsStory">
+	<div style="padding:40px;">
+		{#each colors as color}
+			<Group mt="xl">
+				<ActionIcon {color} />
+				<ActionIcon {color} variant="filled" />
+				<ActionIcon {color} variant="outline" />
+				<ActionIcon {color} variant="light" />
+				<ActionIcon {color} variant="default" />
+				<ActionIcon {color} variant="transparent" />
+			</Group>
+		{/each}
+	</div>
+</Story>
+
+<Story name="Disabled" id="actionDisabledStory" template="variants" args={{ disabled: true }} />
+
+<Story name="Loading" id="actionLoadingStory" template="variants" args={{ loading: true }} />
--- packages/svelteui-core/src/components/Button/Button.stories.svelte
+++ packages/svelteui-core/src/components/Button/Button.stories.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
 	import { LockClosed } from 'radix-icons-svelte';
-	import { Button } from './index';
-	import { Group } from '../Group';
 	import { useSvelteUITheme } from '$lib/styles';
+	import { Group } from '../Group';
+	import { Button } from './index';
 
 	const theme = useSvelteUITheme();
 	const colors = Object.keys(theme.colorNames);
@@ -13,6 +13,18 @@
 
 <Template let:args>
 	<Button {...args} />
+</Template>
+
+<Template id="variants" let:args>
+	<Group mt="xl">
+    <Button {...args} variant="filled">Filled button</Button>
+    <Button {...args} variant="light">Light button</Button>
+    <Button {...args} variant="outline">Outline button</Button>
+    <Button {...args} variant="default">Default button</Button>
+    <Button {...args} variant="white">White button</Button>
+    <Button {...args} variant="gradient">Gradient button</Button>
+    <Button {...args} variant="subtle">Subtle button</Button>
+  </Group>
 </Template>
 
 <Story name="Button" id="buttonStory" />
@@ -37,4 +49,8 @@
 	</Button>
 </Story>
 
-<Story name="Disabled" id="buttonDisabledStory" args={{ disabled: true }} />
+<Story name="Disabled" id="buttonDisabledStory" template="variants" args={{ disabled: true }} />
+
+<Story name="Loading" id="buttonLoadingStory" template="variants" args={{ loading: true }} />
+
+<Story name="With href" id="buttonHrefStory" args={{ href: 'https://www.svelteui.org', external: true, disabled: false }} />
