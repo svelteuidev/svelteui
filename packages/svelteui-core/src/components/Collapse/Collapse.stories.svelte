6fc14a602fdb990aea59372387e5d1bc3c20af7f
Margarida Silva <ams-amargarida@hotmail.com>
2023-06-04 20:39:53 +0100

    [feat](@svelteui/core): collapse component

---
+++ packages/svelteui-core/src/components/Collapse/Collapse.stories.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import { Button } from '../Button';
+	import { Paper } from '../Paper';
+	import { Collapse } from './index';
+
+  let open = false;
+</script>
+
+<Meta title="Components/Collapse" component={Collapse} />
+
+<Template let:args>
+  <Button on:click={() => { open = !open }}>Toggle collapse text</Button>
+
+  <Collapse {open} {...args}>
+    <Paper>This is a hidden text!</Paper>
+  </Collapse>
+
+  <div>This is a cool text!</div>
+</Template>
+
+<Story name="Collapse" id="collapseStory" />
---
+++ packages/svelteui-core/src/components/Collapse/Collapse.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import { fade, slide } from 'svelte/transition';
+  import { createEventForwarder } from '$lib/internal';
+	import { get_current_component } from 'svelte/internal';
+  import { Box } from '../Box';
+	import type { CollapseProps as $$CollapseProps } from './Collapse';
+
+	interface $$Props extends $$CollapseProps {}
+
+	export let use: $$Props['use'] = [],
+		element: $$Props['element'] = undefined,
+		className: $$Props['className'] = '',
+		override: $$Props['override'] = {},
+    open: $$Props['open'],
+    animateOpacity: $$Props['animateOpacity'] = true,
+    transitionDuration: $$Props['transitionDuration'] = 200,
+    slideTransitionOptions: $$Props['slideTransitionOptions'] = { duration: transitionDuration };
+	export { className as class };
+
+  const forwardEvents = createEventForwarder(get_current_component());
+</script>
+
+<!--
+@component
+
+A section that collapses with animation, occupying no space on the page.
+
+@see https://svelteui.org/core/collapse
+@example
+    ```svelte
+    <Collapse open>Content that collapses</Collapse>
+    ```
+-->
+<Box
+  class={className}
+  bind:element
+  css={override}
+  {use}
+>
+  {#if open}
+    {#if animateOpacity}
+      <div transition:fade={{ duration: transitionDuration }}>
+        <div transition:slide={slideTransitionOptions} use:forwardEvents>
+          <slot />
+        </div>
+      </div>
+    {:else}
+      <div transition:slide={slideTransitionOptions} use:forwardEvents>
+        <slot />
+      </div>
+    {/if}
+  {/if}
+</Box>
