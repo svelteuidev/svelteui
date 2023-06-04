1a03b76ca1f164c61a51642819bd047f1c01b360
Margarida Silva <ams-amargarida@hotmail.com>
2023-06-04 20:46:00 +0100

    [feat](@svelteui/core): collapse support for nested collapses

--- packages/svelteui-core/src/components/Collapse/Collapse.stories.svelte
+++ packages/svelteui-core/src/components/Collapse/Collapse.stories.svelte
@@ -5,6 +5,7 @@
 	import { Collapse } from './index';
 
   let open = false;
+  let openInside = false;
 </script>
 
 <Meta title="Components/Collapse" component={Collapse} />
@@ -20,3 +21,20 @@
 </Template>
 
 <Story name="Collapse" id="collapseStory" />
+
+<Story name="Nested Collapse" id="collapseNestedStory">
+  <Button on:click={() => { open = !open }}>Toggle collapse</Button>
+
+  <Collapse {open}>
+    <Paper>
+      Please click below to toggle a nested collapse!
+      <Button on:click={() => { openInside = !openInside }}>Toggle nested collapse</Button>
+
+      <Collapse open={openInside}>
+        <Paper>This is a very hidden text, sshhhh!</Paper>
+      </Collapse>
+    </Paper>
+  </Collapse>
+
+  <div>Footer text</div>
+</Story>
