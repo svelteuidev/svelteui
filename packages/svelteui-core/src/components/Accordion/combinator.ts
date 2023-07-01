8352b24377c1fbd06f7fb8706fa19f9161b37fe0
Margarida Silva <ams-amargarida@hotmail.com>
2023-07-01 14:28:33 +0100

    [fix](@svelteui/core): remove unused import

--- packages/svelteui-core/src/components/Accordion/combinator.ts
+++ packages/svelteui-core/src/components/Accordion/combinator.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import _Accordion from './Accordion.svelte';
-import AccordionControl from './AccordionControl/AccordionControl.svelte';
 import AccordionItem from './AccordionItem/AccordionItem.svelte';
 
 // @ts-ignore
@@ -8,7 +7,6 @@ _Accordion.Control = AccordionControl;
 // @ts-ignore
 _Accordion.Item = AccordionItem;
 const Accordion = _Accordion as typeof _Accordion & {
-	Control: typeof AccordionControl;
 	Item: typeof AccordionItem;
 };
 
