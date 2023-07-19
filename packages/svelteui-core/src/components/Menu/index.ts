718a1f9ca7d92742bb309fbba46fdf7d72b2493e
Ana Margarida <ams-amargarida@hotmail.com>
2023-07-19 20:14:27 +0100

    [fix](@svelteui/core): allow export Menu type to correctly type 'this' prop

--- packages/svelteui-core/src/components/Menu/index.ts
+++ packages/svelteui-core/src/components/Menu/index.ts
@@ -2,6 +2,9 @@ export { Menu } from './combinator';
 export type { MenuStyleParams } from './Menu.styles';
 export type { MenuProps, MenuEvents } from './Menu';
 
+import type MenuComponent from './Menu.svelte';
+export type { MenuComponent };
+
 export { default as MenuIcon } from './MenuIcon.svelte';
 export type { MenuItemStylesParams } from './MenuItem/MenuItem.styles';
 export type { MenuItemProps, SharedMenuItemProps } from './MenuItem/MenuItem';
