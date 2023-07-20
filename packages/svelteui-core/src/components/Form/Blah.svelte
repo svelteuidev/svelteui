c6ddb881ac981d776dffaf24eb14bd4d5399bcf6
Khalifa Lame <khalibloo@gmail.com>
2023-07-21 02:55:15 +0400

    [@svelteui/core]: test form list items

--- packages/svelteui-core/src/components/Form/Blah.svelte
+++ packages/svelteui-core/src/components/Form/Blah.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { Trash } from 'radix-icons-svelte';
 	import { Button } from '../Button';
 	import Center from '../Center/Center.svelte';
 	import Checkbox from '../Checkbox/Checkbox.svelte';
@@ -11,10 +12,21 @@
 </script>
 
 <Form
-	initialValues={{ firstName: 'Super', lastName: '', agree: false }}
+	initialValues={{
+		user: { firstName: 'Super', lastName: '' },
+		pokemon: [
+			{ name: 'Mantine', type: 'water' },
+			{ name: 'Monferno', type: 'fire' },
+			{ name: 'Magicarp', type: 'water' }
+		],
+		agree: false
+	}}
 	validation={{
-		firstName: isNotEmpty('First name is required'),
-		lastName: hasLength({ min: 3 }, 'Value must have 3  or more characters'),
+		user: {
+			firstName: isNotEmpty('First name is required'),
+			lastName: hasLength({ min: 3 }, 'Value must have 3  or more characters')
+		},
+		pokemon: { name: isNotEmpty('Name is required') },
 		agree: (v) => (v ? undefined : 'You must agree to the terms we know you didnt read')
 	}}
 	validateInputOnChange
@@ -29,7 +41,7 @@
 	>
 		<Center>
 			<Stack>
-				<Field {form} name="firstName" let:field let:htmlInputProps>
+				<Field {form} name="user.firstName" let:field let:htmlInputProps>
 					<label>
 						<div>First name (html input)</div>
 						<input
@@ -42,10 +54,41 @@
 						<div id="first-name-error">{field.error}</div>
 					{/if}
 				</Field>
-				<Field {form} name="lastName" let:field let:inputProps>
+				<Field {form} name="user.lastName" let:field let:inputProps>
 					<TextInput label="Last name (svelteui input)" {...inputProps} on:input={field.onChange} />
-					{field.value}
 				</Field>
+				<h2>Pokemon</h2>
+				{#each form.values.pokemon as pokemon, i}
+					<Flex gap="$md" align="end">
+						<Field {form} name={`pokemon.${i}.name`} let:field let:inputProps>
+							<TextInput label="Name" {...inputProps} on:input={field.onChange} />
+						</Field>
+						<Field {form} name={`pokemon.${i}.type`} let:field let:inputProps>
+							<TextInput label="Type" {...inputProps} on:input={field.onChange} />
+						</Field>
+						<Button
+							on:click={() => form.removeListItem('pokemon', i)}
+							type="button"
+							variant="default"><Trash /></Button
+						>
+					</Flex>
+				{/each}
+				<Flex gap="$md">
+					<Button
+						on:click={() => form.insertListItem('pokemon', { name: '', type: '' })}
+						type="button"
+					>
+						Add Pokemon
+					</Button>
+					<Button
+						disabled={!form.values.pokemon.length}
+						on:click={() =>
+							form.reorderListItem('pokemon', { from: form.values.pokemon.length - 1, to: 0 })}
+						type="button"
+					>
+						Move Last Item To The Top
+					</Button>
+				</Flex>
 				<Field {form} name="agree" isCheckbox let:field let:inputProps>
 					<Checkbox
 						aria-describedby={field.error ? 'agree-error' : undefined}
