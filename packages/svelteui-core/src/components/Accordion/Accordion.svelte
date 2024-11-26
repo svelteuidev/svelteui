bd877825e7c4d7e87867159c71967cf835e90783
BeeMargarida <ana.margarida.silva@protonmail.com>
2024-11-26 23:44:23 +0000

    wip: start working on migrating Accordion

--- packages/svelteui-core/src/components/Accordion/Accordion.svelte
+++ packages/svelteui-core/src/components/Accordion/Accordion.svelte
@@ -3,47 +3,40 @@
 <script lang="ts" generics="Multiple extends boolean = false">
 	// Temporary eslint disable while svelte-eslint-parser does not support generics in file -->
 	/* eslint-disable no-undef */
-	import { createEventDispatcher, setContext } from 'svelte';
+	import { setContext } from 'svelte';
 	import { writable } from 'svelte/store';
 	import { randomID } from '$lib/styles';
 	import { Box } from '../Box';
 	import { key } from './key';
 	import Chevron from './Chevron/Chevron.svelte';
 	import useStyles from './Accordion.styles';
-	import type {
-		AccordionContext,
-		AccordionProps as $$AccordionProps,
-		AccordionEvents as $$AccordionEvents,
-		AccordionValue
-	} from './Accordion';
+	import type { AccordionContext, AccordionProps, AccordionValue } from './Accordion';
 
-	interface $$Props extends $$AccordionProps<Multiple> {}
-
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	interface $$Events extends $$AccordionEvents<Multiple> {}
-
-	export let use: $$Props['use'] = [],
-		element: $$Props['element'] = undefined,
-		className: $$Props['className'] = '',
-		override: $$Props['override'] = {},
-		variant: $$Props['variant'] = 'default',
-		value: $$Props['value'] = undefined,
-		defaultValue: $$Props['defaultValue'] = undefined,
-		radius: $$Props['radius'] = 'sm',
-		order: $$Props['order'] = undefined,
-		multiple: $$Props['multiple'] = false as Multiple,
-		id: $$Props['id'] = randomID(),
-		chevron: $$Props['chevron'] = Chevron,
-		chevronPosition: $$Props['chevronPosition'] = 'right',
-		chevronSize: $$Props['chevronSize'] = 24,
-		disableChevronRotation: $$Props['disableChevronRotation'] = false,
-		transitionDuration: $$Props['transitionDuration'] = 200;
-	export { className as class };
-
-	const dispatch = createEventDispatcher();
+	let {
+		use = [],
+		element = $bindable(undefined),
+		class: className = '',
+		override = {},
+		variant = 'default',
+		value = $bindable(undefined),
+		defaultValue = undefined,
+		radius = 'sm',
+		order = undefined,
+		multiple = false as Multiple,
+		id = randomID(),
+		chevron = Chevron, // @TODO: this should be a snippet?
+		chevronPosition = 'right',
+		chevronSize = 24,
+		disableChevronRotation = false,
+		transitionDuration = 200,
+		onchange = () => {},
+		children,
+		...rest
+	}: AccordionProps<Multiple> = $props();
 
 	let _value: AccordionValue<Multiple>;
-	let stateContent = {
+	let context = $derived({
+		currentValue: value,
 		variant,
 		order,
 		radius,
@@ -53,42 +46,16 @@
 		disableChevronRotation,
 		transitionDuration,
 		updateActive,
-		isItemActive
-	};
-	const state = writable(stateContent);
-
-	// converts internal value into correct type
-	$: _value = value || defaultValue;
-	$: {
-		if (multiple && !Array.isArray(_value)) {
-			_value = (_value ? [_value] : []) as AccordionValue<Multiple>;
-		}
-	}
-
-	// allows for reactivity to prop changes
-	$: {
-		stateContent = {
-			currentValue: _value,
-			variant,
-			order,
-			radius,
-			chevron,
-			chevronPosition,
-			chevronSize,
-			disableChevronRotation,
-			transitionDuration,
-			updateActive,
-			isItemActive,
-			getControlsId,
-			getRegionId
-		} as AccordionContext;
-		state.set(stateContent);
-	}
+		isItemActive,
+		getControlsId,
+		getRegionId
+	});
 
 	function updateActive(itemValue: string) {
 		if (!multiple) {
 			_value = (_value === itemValue ? undefined : itemValue) as AccordionValue<Multiple>;
-			dispatch('change', _value);
+
+			onchange(_value);
 			return;
 		}
 
@@ -99,7 +66,7 @@
 			values.push(itemValue);
 		}
 		_value = values as AccordionValue<Multiple>;
-		dispatch('change', _value);
+		onchange(_value);
 	}
 
 	function isItemActive(itemValue: string) {
@@ -114,41 +81,9 @@
 		return `${id}-${itemValue}-region`;
 	}
 
-	setContext(key, state);
-
-	$: ({ cx, classes, getStyles } = useStyles({ radius, variant }, { name: 'Accordion' }));
+	let { cx, classes, getStyles } = $derived(useStyles({ radius, variant }, { name: 'Accordion' }));
 </script>
 
-<!--
-@component
-
-Accordion component that allows the collapsing of each item.
-
-@see https://svelteui.dev/core/accordion
-@example
-    ```svelte
-    <Accordion defaultValue="typescript">
-      <Accordion.Item value="typescript">
-        <div slot="control">Typescript Based</div>
-        Content of the accordion item
-      </Accordion.Item>
-      <Accordion.Item value="packed" disabled>
-        <div slot="control">Feature packed</div>
-        ...
-      </Accordion.Item>
-      <Accordion.Item value="accessible">
-        <div slot="control">Accessible and usable</div>
-        ...
-      </Accordion.Item>
-    </Accordion>
-    ```
--->
-
-<Box
-	class={cx(className, classes.root, getStyles({ css: override }))}
-	bind:element
-	{use}
-	{...$$restProps}
->
-	<slot />
+<Box bind:element class={cx(className, classes.root, getStyles({ css: override }))} {use} {...rest}>
+	{@render children(context)}
 </Box>
--- packages/svelteui-core/src/components/Accordion/Accordion.ts
+++ packages/svelteui-core/src/components/Accordion/Accordion.ts
@@ -1,12 +1,15 @@
-import { HTMLAttributes } from 'svelte/elements';
-import { Component } from '$lib/internal';
+import type { Snippet } from 'svelte';
+import type { Writable } from 'svelte/store';
+import type { HTMLAttributes } from 'svelte/elements';
+
+import type { Component } from '$lib/internal';
 import type { DefaultProps, SvelteUINumberSize } from '$lib/styles';
 
 export type AccordionVariant = 'default' | 'contained' | 'filled' | 'separated';
 
 export type AccordionValue<Multiple> = Multiple extends true ? string[] : string | null;
 
-export type AccordionContext<Multiple extends boolean = false> = Writable<{
+export type AccordionContext<Multiple extends boolean = false> = {
 	currentValue?: AccordionValue<Multiple>;
 	variant?: AccordionVariant;
 	order?: 2 | 3 | 4 | 5 | 6;
@@ -15,12 +18,12 @@ export type AccordionContext<Multiple extends boolean = false> = Writable<{
 	chevronPosition?: 'left' | 'right';
 	chevronSize?: string | number;
 	disableChevronRotation?: boolean;
-	transitionDuration?: number?;
+	transitionDuration?: number;
 	updateActive: (value: string) => void;
 	isItemActive: (value: string) => boolean;
 	getControlsId: (value: string) => string;
 	getRegionId: (value: string) => string;
-}>;
+};
 
 export interface AccordionProps<Multiple extends boolean = false>
 	extends DefaultProps,
@@ -37,7 +40,10 @@ export interface AccordionProps<Multiple extends boolean = false>
 	chevronPosition?: 'left' | 'right';
 	chevronSize?: string | number;
 	disableChevronRotation?: boolean;
-	transitionDuration?: number?;
+	transitionDuration?: number;
+	onchange: (value: AccordionValue<Multiple>) => void;
+	children: Snippet<[AccordionContext<Multiple>]>;
+	[key: string]: any;
 }
 
 export interface AccordionEvents<Multiple extends boolean = false> {
