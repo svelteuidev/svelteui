e10bdc499d66edf5effcaf5065460339982fbbee
BeeMargarida <ams-amargarida@hotmail.com>
2024-12-08 22:17:10 +0000

    fix: migrate Container

--- packages/svelteui-core/src/components/Container/Container.svelte
+++ packages/svelteui-core/src/components/Container/Container.svelte
@@ -1,21 +1,7 @@
 <script lang="ts">
 	import useStyles from './Container.styles';
 	import Box from '../Box/Box.svelte';
-	import type { ContainerProps as $$ContainerProps } from './Container';
-
-	
-
-	interface Props {
-		use?: $$Props['use'];
-		element?: $$Props['element'];
-		class?: $$Props['className'];
-		override?: $$Props['override'];
-		size?: $$Props['size'];
-		fluid?: $$Props['fluid'];
-		sizes?: $$Props['sizes'];
-		children?: import('svelte').Snippet;
-		[key: string]: any
-	}
+	import type { ContainerProps } from './Container';
 
 	let {
 		use = [],
@@ -33,38 +19,13 @@
 		},
 		children,
 		...rest
-	}: Props = $props();
-	
+	}: ContainerProps = $props();
 
-	let { cx, classes, getStyles } = $derived(useStyles({ fluid, size, sizes }, { name: 'Container' }));
+	let { cx, classes, getStyles } = $derived(
+		useStyles({ fluid, size, sizes }, { name: 'Container' })
+	);
 </script>
 
-<!--
-@component
-
-Center content horizontally with predefined max-width
-
-@see https://svelteui.dev/core/container
-@example
-    ```svelte
-    <Container>
-        Default container
-    </Container>
-
-    <Container size="xs" px="xs">
-        xs container with xs horizontal padding
-    </Container>
-
-    <Container size={200} px={0}>
-        200px container with 0px horizontal padding
-    </Container>
-    ```
--->
-<Box
-	bind:element
-	{use}
-	class={cx(className, classes.root, getStyles({ css: override }))}
-	{...rest}
->
+<Box bind:element {use} class={cx(className, classes.root, getStyles({ css: override }))} {...rest}>
 	{@render children?.()}
 </Box>
--- packages/svelteui-core/src/components/Container/Container.ts
+++ packages/svelteui-core/src/components/Container/Container.ts
@@ -1,8 +1,11 @@
-import { HTMLAttributes } from 'svelte/elements';
-import { DefaultProps, SvelteUINumberSize, SvelteUISize } from '$lib/styles';
+import type { Snippet } from 'svelte';
+import type { HTMLAttributes } from 'svelte/elements';
+import type { DefaultProps, SvelteUINumberSize, SvelteUISize } from '$lib/styles';
 
 export interface ContainerProps extends DefaultProps, HTMLAttributes<HTMLElement> {
 	size?: SvelteUINumberSize;
 	fluid?: boolean;
 	sizes?: Record<SvelteUISize, number>;
+	children?: Snippet;
+	[key: string]: any
 }
