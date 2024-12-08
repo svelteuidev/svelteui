b0cc34528e465fccd0f0403892fb88a20d392fc4
BeeMargarida <ams-amargarida@hotmail.com>
2024-12-08 18:33:06 +0000

    fix: migrate AspectRatio

--- packages/svelteui-core/src/components/AspectRatio/AspectRatio.svelte
+++ packages/svelteui-core/src/components/AspectRatio/AspectRatio.svelte
@@ -1,19 +1,7 @@
 <script lang="ts">
 	import useStyles from './AspectRatio.styles';
 	import Box from '../Box/Box.svelte';
-	import type { AspectRatioProps as $$AspectRatioProps } from './AspectRatio';
-
-	
-
-	interface Props {
-		use?: $$Props['use'];
-		element?: $$Props['element'];
-		class?: $$Props['className'];
-		override?: $$Props['override'];
-		ratio?: $$Props['ratio'];
-		children?: import('svelte').Snippet;
-		[key: string]: any
-	}
+	import type { AspectRatioProps } from './AspectRatio';
 
 	let {
 		use = [],
@@ -23,39 +11,18 @@
 		ratio = 1,
 		children,
 		...rest
-	}: Props = $props();
-	
+	}: AspectRatioProps = $props();
 
-	let { cx, classes, getStyles } = $derived(useStyles(
-		{
-			ratio
-		},
-		{ name: 'AspectRatio' }
-	));
+	let { cx, classes, getStyles } = $derived(
+		useStyles(
+			{
+				ratio
+			},
+			{ name: 'AspectRatio' }
+		)
+	);
 </script>
 
-<!--
-@component
-
-Enforce desired width/height ratio.
-
-@see https://svelteui.dev/core/aspect-ratio
-@example
-  ```svelte
-    <AspectRatio ratio={16 / 9}>
-		  <Image
-        src="https://images.unsplash.com/photo-1527118732049-c88155f2107c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80"
-        alt="Panda"
-      />
-	  </AspectRatio>
-  ```
--->
-
-<Box
-	bind:element
-	{use}
-	class={cx(className, classes.root, getStyles({ css: override }))}
-	{...rest}
->
+<Box bind:element {use} class={cx(className, classes.root, getStyles({ css: override }))} {...rest}>
 	{@render children?.()}
 </Box>
--- packages/svelteui-core/src/components/AspectRatio/AspectRatio.ts
+++ packages/svelteui-core/src/components/AspectRatio/AspectRatio.ts
@@ -1,6 +1,10 @@
-import { HTMLAttributes } from 'svelte/elements';
-import { DefaultProps } from '$lib/styles';
+import type { Snippet } from 'svelte';
+import type { HTMLAttributes } from 'svelte/elements';
+
+import type { DefaultProps } from '$lib/styles';
 
 export interface AspectRatioProps extends DefaultProps, HTMLAttributes<HTMLElement> {
 	ratio: number;
+	children?: Snippet;
+	[key: string]: any
 }
