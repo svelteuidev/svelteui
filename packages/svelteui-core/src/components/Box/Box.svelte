098e40e458f84df1bc94658a41e021b091db70e9
BeeMargarida <ams-amargarida@hotmail.com>
2024-12-08 18:33:30 +0000

    fix: migrate Box

--- packages/svelteui-core/src/components/Box/Box.svelte
+++ packages/svelteui-core/src/components/Box/Box.svelte
@@ -1,37 +1,8 @@
 <script lang="ts">
-	import { run } from 'svelte/legacy';
-
-	import { getSystemStyles } from './context';
 	import { css as _css, useSvelteUITheme, useSvelteUIThemeContext } from '$lib/styles';
-	import { createEventForwarder, useActions } from '$lib/internal';
-	import { get_current_component } from 'svelte/internal';
-	import type { BoxProps as $$BoxProps } from './Box';
-
-	
-
-	interface Props {
-		use?: $$Props['use'];
-		element?: $$Props['element'];
-		class?: $$Props['className'];
-		css?: $$Props['css'];
-		root?: $$Props['root'];
-		m?: $$Props['m'];
-		my?: $$Props['my'];
-		mx?: $$Props['mx'];
-		mt?: $$Props['mt'];
-		mb?: $$Props['mb'];
-		ml?: $$Props['ml'];
-		mr?: $$Props['mr'];
-		p?: $$Props['p'];
-		py?: $$Props['py'];
-		px?: $$Props['px'];
-		pt?: $$Props['pt'];
-		pb?: $$Props['pb'];
-		pl?: $$Props['pl'];
-		pr?: $$Props['pr'];
-		children?: import('svelte').Snippet;
-		[key: string]: any
-	}
+	import { useActions } from '$lib/internal';
+	import { getSystemStyles } from './context';
+	import type { BoxProps } from './Box';
 
 	let {
 		use = [],
@@ -55,64 +26,45 @@
 		pr = undefined,
 		children,
 		...rest
-	}: Props = $props();
-	
+	}: BoxProps = $props();
 
-	/** An action that forwards inner dom node events from parent component */
-	const forwardEvents = createEventForwarder(get_current_component());
 	/** workaround for root type errors, this should be replaced by a better type system */
 	const castRoot = () => root as string;
 	const theme = useSvelteUIThemeContext()?.theme || useSvelteUITheme();
 	let getCSSStyles = $derived(typeof css === 'function' ? css : () => css);
 
-	let isHTMLElement = $state();
-	let isComponent = $state();
-	run(() => {
-		isHTMLElement = root && typeof root === 'string';
-		isComponent = root && typeof root === 'function';
-	});
+	let isHTMLElement = $derived(root && typeof root === 'string');
+	let isComponent = $derived(root && typeof root === 'function');
+
 	let BoxStyles = $derived(_css({}));
-	let systemStyles = $derived(getSystemStyles(
-		{
-			m,
-			my,
-			mx,
-			mt,
-			mb,
-			ml,
-			mr,
-			p,
-			py,
-			px,
-			pt,
-			pb,
-			pl,
-			pr
-		},
-		theme
-	));
+	let systemStyles = $derived(
+		getSystemStyles(
+			{
+				m,
+				my,
+				mx,
+				mt,
+				mb,
+				ml,
+				mr,
+				p,
+				py,
+				px,
+				pt,
+				pb,
+				pl,
+				pr
+			},
+			theme
+		)
+	);
 </script>
-
-<!--
-@component
-
-Add inline styles to any element or component with sx.
-
-@see https://svelteui.dev/core/box
-@example
-    ```svelte
-    <Box css={{backgroundColor: '$blue600'}}>example</Box>
-	<Box root='span'>example</Box>
-	<Box css={{pt: '$20'}}>padding top of 5rem</Box> // uses the paddingTop shorthand, and theme scale
-    ```
--->
 
 {#if isHTMLElement}
 	<!-- prettier-ignore -->
 	<svelte:element
 		bind:this={element}
 		this={castRoot()}
-		use:forwardEvents
 		use:useActions={use}
 		class="{className} {BoxStyles({ css: {...getCSSStyles(theme), ...systemStyles} })}"
 		{...rest}
@@ -123,7 +75,7 @@ Add inline styles to any element or component with sx.
 	{@const SvelteComponent = root}
 	<SvelteComponent
 		bind:this={element}
-		use={[forwardEvents, [useActions, use]]}
+		use={[[useActions, use]]}
 		class="{className} {BoxStyles({ css: { ...getCSSStyles(theme), ...systemStyles } })}"
 		{...rest}
 	>
@@ -132,7 +84,6 @@ Add inline styles to any element or component with sx.
 {:else}
 	<div
 		bind:this={element}
-		use:forwardEvents
 		use:useActions={use}
 		class="{className} {BoxStyles({ css: { ...getCSSStyles(theme), ...systemStyles } })}"
 		{...rest}
