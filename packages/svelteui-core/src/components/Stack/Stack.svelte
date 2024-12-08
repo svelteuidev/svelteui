7f9cfddef64bc00bf7f874066f647994aba6859f
BeeMargarida <ams-amargarida@hotmail.com>
2024-12-08 19:03:16 +0000

    fix: migrate Stack

--- packages/svelteui-core/src/components/Stack/Stack.svelte
+++ packages/svelteui-core/src/components/Stack/Stack.svelte
@@ -1,21 +1,7 @@
 <script lang="ts">
 	import Box from '../Box/Box.svelte';
 	import useStyles from './Stack.styles';
-	import type { StackProps as $$StackProps } from './Stack';
-
-	
-
-	interface Props {
-		use?: $$Props['use'];
-		element?: $$Props['element'];
-		class?: $$Props['className'];
-		override?: $$Props['override'];
-		spacing?: $$Props['spacing'];
-		align?: $$Props['align'];
-		justify?: $$Props['justify'];
-		children?: import('svelte').Snippet;
-		[key: string]: any
-	}
+	import type { StackProps } from './Stack';
 
 	let {
 		use = [],
@@ -27,33 +13,13 @@
 		justify = 'center',
 		children,
 		...rest
-	}: Props = $props();
-	
+	}: StackProps = $props();
 
-	let { cx, classes, getStyles } = $derived(useStyles({ align, justify, spacing }, { name: 'Stack' }));
+	let { cx, classes, getStyles } = $derived(
+		useStyles({ align, justify, spacing }, { name: 'Stack' })
+	);
 </script>
 
-<!--
-@component
-
-Compose elements and components in a vertical flex container.
-
-@see https://svelteui.dev/core/stack
-@example
-    ```svelte
-    <Stack align="flex-end" justify="space-between" override={{ height: 300 }}>
-		<Button variant="outline">1</Button>
-		<Button variant="outline">2</Button>
-		<Button variant="outline">3</Button>
-	</Stack>
-    ```
--->
-
-<Box
-	bind:element
-	{use}
-	class={cx(className, classes.root, getStyles({ css: override }))}
-	{...rest}
->
+<Box bind:element {use} class={cx(className, classes.root, getStyles({ css: override }))} {...rest}>
 	{@render children?.()}
 </Box>
--- packages/svelteui-core/src/components/Stack/Stack.ts
+++ packages/svelteui-core/src/components/Stack/Stack.ts
@@ -1,8 +1,11 @@
-import { HTMLAttributes } from 'svelte/elements';
-import { CSS, DefaultProps, SvelteUINumberSize } from '$lib/styles';
+import type { Snippet } from 'svelte';
+import type { HTMLAttributes } from 'svelte/elements';
+import type { CSS, DefaultProps, SvelteUINumberSize } from '$lib/styles';
 
 export interface StackProps extends DefaultProps, HTMLAttributes<HTMLElement> {
 	spacing?: SvelteUINumberSize;
 	align?: CSS['alignItems'];
 	justify?: CSS['justifyContent'];
+	children?: Snippet;
+	[key: string]: any
 }
