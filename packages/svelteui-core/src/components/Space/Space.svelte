48c89b50686232fed3c92143e311df57aefdde87
BeeMargarida <ams-amargarida@hotmail.com>
2024-12-08 19:03:09 +0000

    fix: migrate Space

--- packages/svelteui-core/src/components/Space/Space.svelte
+++ packages/svelteui-core/src/components/Space/Space.svelte
@@ -1,20 +1,7 @@
 <script lang="ts">
 	import { Box } from '../Box';
 	import useStyles from './Space.styles';
-	import type { SpaceProps as $$SpaceProps } from './Space';
-
-	
-
-	interface Props {
-		use?: $$Props['use'];
-		element?: $$Props['element'];
-		class?: $$Props['className'];
-		override?: $$Props['override'];
-		w?: $$Props['w'];
-		h?: $$Props['h'];
-		children?: import('svelte').Snippet;
-		[key: string]: any
-	}
+	import type { SpaceProps } from './Space';
 
 	let {
 		use = [],
@@ -25,30 +12,11 @@
 		h = 0,
 		children,
 		...rest
-	}: Props = $props();
-	
+	}: SpaceProps = $props();
 
 	let { cx, classes, getStyles } = $derived(useStyles({ h, w }, { name: 'Space' }));
 </script>
 
-<!--
-@component
-
-Add horizontal or vertical spacing from theme.
-
-@see https://svelteui.dev/core/space
-@example
-    ```svelte
-		<Space h="md" />
-		<Space w="lg" />
-		<Space w={30} /> // Width will be set to 30px
-    ```
--->
-<Box
-	bind:element
-	{use}
-	class={cx(className, classes.root, getStyles({ css: override }))}
-	{...rest}
->
+<Box bind:element {use} class={cx(className, classes.root, getStyles({ css: override }))} {...rest}>
 	{@render children?.()}
 </Box>
--- packages/svelteui-core/src/components/Space/Space.ts
+++ packages/svelteui-core/src/components/Space/Space.ts
@@ -1,7 +1,10 @@
-import { HTMLAttributes } from 'svelte/elements';
-import { DefaultProps, SvelteUINumberSize } from '$lib/styles';
+import type { Snippet } from 'svelte';
+import type  { HTMLAttributes } from 'svelte/elements';
+import type { DefaultProps, SvelteUINumberSize } from '$lib/styles';
 
 export interface SpaceProps extends DefaultProps, HTMLAttributes<HTMLElement> {
 	w?: SvelteUINumberSize;
 	h?: SvelteUINumberSize;
+	children?: Snippet;
+	[key: string]: any
 }
