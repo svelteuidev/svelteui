3a55fdc10b393c0234ac0ac459402c05db2c488f
BeeMargarida <ams-amargarida@hotmail.com>
2024-12-28 19:14:14 +0000

    wip: migrate TypographyProvider

--- packages/svelteui-core/src/components/TypographyProvider/TypographyProvider.svelte
+++ packages/svelteui-core/src/components/TypographyProvider/TypographyProvider.svelte
@@ -1,20 +1,7 @@
 <script lang="ts">
 	import { Box } from '../Box';
 	import useStyles from './TypographyProvider.styles';
-	import type { TypographyProviderProps as $$TypographyProviderProps } from './TypographyProvider';
-
-	
-
-	interface Props {
-		use?: $$Props['use'];
-		element?: $$Props['element'];
-		class?: $$Props['className'];
-		override?: $$Props['override'];
-		primaryColor?: $$Props['primaryColor'];
-		underline?: $$Props['underline'];
-		children?: import('svelte').Snippet;
-		[key: string]: any
-	}
+	import type { TypographyProviderProps } from './TypographyProvider';
 
 	let {
 		use = [],
@@ -25,20 +12,13 @@
 		underline = true,
 		children,
 		...rest
-	}: Props = $props();
-	
+	}: TypographyProviderProps = $props();
 
-	let { cx, classes, getStyles } = $derived(useStyles(
-		{ primaryColor, underline },
-		{ name: 'TypographyProvider' }
-	));
+	let { cx, classes, getStyles } = $derived(
+		useStyles({ primaryColor, underline }, { name: 'TypographyProvider' })
+	);
 </script>
 
-<Box
-	bind:element
-	class={cx(className, classes.root, getStyles({ css: override }))}
-	{use}
-	{...rest}
->
+<Box bind:element class={cx(className, classes.root, getStyles({ css: override }))} {use} {...rest}>
 	{@render children?.()}
 </Box>
--- packages/svelteui-core/src/components/TypographyProvider/TypographyProvider.ts
+++ packages/svelteui-core/src/components/TypographyProvider/TypographyProvider.ts
@@ -1,7 +1,11 @@
-import { HTMLAttributes } from 'svelte/elements';
-import { DefaultProps, SvelteUIColor } from '$lib/styles';
+import type { Snippet } from 'svelte';
+import type { HTMLAttributes } from 'svelte/elements';
+
+import type { DefaultProps, SvelteUIColor } from '$lib/styles';
 
 export interface TypographyProviderProps extends DefaultProps, HTMLAttributes<HTMLElement> {
 	underline?: boolean;
 	primaryColor?: SvelteUIColor;
+	children?: Snippet;
+	[key: string]: any
 }
