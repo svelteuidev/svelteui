5ce5448de7b7592b3dcf2b02fad6a7f13ab6c31f
BeeMargarida <ams-amargarida@hotmail.com>
2024-12-28 20:05:23 +0000

    wip: ran sk2 migration script in docs

--- apps/docs/svelte.config.js
+++ apps/docs/svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { mdsvex } from 'mdsvex';
