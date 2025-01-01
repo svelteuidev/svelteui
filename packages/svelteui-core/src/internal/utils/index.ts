02dec85b2d890d4b5dab7815100b049dcd767c0d
Ana Silva <--list>
2025-01-01 23:31:21 +0000

    fix: remove dead code

--- packages/svelteui-core/src/internal/utils/index.ts
+++ packages/svelteui-core/src/internal/utils/index.ts
@@ -1,4 +1,3 @@
 export { useActions } from './forwarding/forward-actions.js';
-export { createEventForwarder } from './forwarding/forward-events.js';
 export { ENVIRONMENT } from './environment.js';
 export { getTransition } from './get-transition/get-transition.js';
